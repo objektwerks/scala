c661edb7c9bdb897d0fac94028b86bc37809b98f
objektwerks <objektwerks@gmail.com>
2014-07-25 17:42:26 -0400

    added case class test

---
+++ src/test/scala/classes/CaseClassesTest.scala
@@ -0,0 +1,32 @@
+package classes
+
+import org.scalatest.FunSuite
+
+class CaseClassesTest extends FunSuite {
+  trait Animal {
+    def sound(): String
+  }
+
+  case class Tiger(speach: String) extends Animal {
+    def sound(): String = speach
+  }
+
+  case class Shark(speach: String) extends Animal {
+    def sound(): String = speach
+  }
+
+  case class Bear(speach: String) extends Animal {
+    def sound(): String = speach
+  }
+
+  object ZooKeeper {
+    def openCages(): Set[Animal] = Set(new Tiger("prrrr"), new Shark("woosh"), new Bear("grrrr"))
+  }
+
+  test("animals") {
+    val animals = ZooKeeper.openCages()
+    for(animal <- animals ) {
+      assert(animal.sound().length > 0)
+    }
+  }
+}
