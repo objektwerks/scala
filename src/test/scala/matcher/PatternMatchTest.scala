c8fd9a73115a99c164e25c421a392e338f73cdfd
objektwerks <objektwerks@gmail.com>
2014-07-26 18:45:09 -0400

    added pattern match test; and rename test class

---
+++ src/test/scala/matcher/PatternMatchTest.scala
@@ -0,0 +1,24 @@
+package matcher
+
+import org.scalatest.FunSuite
+
+class PatternMatchTest extends FunSuite {
+  test("any match") {
+    def isTrue(a: Any) = a match {
+      case 0 | "" => false
+      case _ => true
+    }
+    assert(isTrue(1))
+    assert(!isTrue(0))
+  }
+
+  test("string match") {
+    def isEqual(x: Int): String = x match {
+      case 1 => "one"
+      case 2 => "two"
+      case _ => "many"
+    }
+    assert(isEqual(1) == "one")
+    assert(isEqual(3) == "many")
+  }
+}
