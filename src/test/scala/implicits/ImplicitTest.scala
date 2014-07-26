4bd5d76871ab9449bc461c1c6c5f6f22221992ea
objektwerks <objektwerks@gmail.com>
2014-07-26 14:38:40 -0400

    added implicit class test

--- src/test/scala/implicits/ImplicitTest.scala
+++ src/test/scala/implicits/ImplicitTest.scala
@@ -3,7 +3,15 @@ package implicits
 import org.scalatest.FunSuite
 
 class ImplicitTest extends FunSuite {
-  test("method extension") {
-
+  test("implicit class") {
+    object Strings {
+      implicit class EnhancedStrings(val s: String) {
+        def toJson = s"{$s}"
+        def toXml = s"<$s>"
+      }
+    }
+    import Strings._
+    assert("json".toJson == "{json}")
+    assert("xml".toXml == "<xml>")
   }
 }
