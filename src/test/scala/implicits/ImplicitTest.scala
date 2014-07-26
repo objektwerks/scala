c399710e4db1f91fbb8231462840cf3ac72586eb
objektwerks <objektwerks@gmail.com>
2014-07-26 19:43:59 -0400

    added implicit parameter test

--- src/test/scala/implicits/ImplicitTest.scala
+++ src/test/scala/implicits/ImplicitTest.scala
@@ -3,6 +3,15 @@ package implicits
 import org.scalatest.FunSuite
 
 class ImplicitTest extends FunSuite {
+  test("implicit parameter") {
+    def order(number: Int) (implicit item: String): String = {
+      s"$number $item"
+    }
+    implicit val item = "beers"
+    assert(order(2) == "2 beers")
+
+  }
+
   test("implicit class") {
     object Strings {
       implicit class EnhancedStrings(val s: String) {
