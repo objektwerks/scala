3d499b9754e511af36449a8c19a38cb9f32388a5
objektwerks <objektwerks@gmail.com>
2014-07-28 07:46:28 -0400

    added return type for clarity

--- src/test/scala/types/TypesTest.scala
+++ src/test/scala/types/TypesTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 
 class TypesTest extends FunSuite {
   test("generic function") {
-    def getMiddle[A](a: Array[A]) = a(a.length / 2)
+    def getMiddle[A](a: Array[A]): A = a(a.length / 2)
     assert(getMiddle(Array("a", "b", "c")) == "b")
   }
 }
