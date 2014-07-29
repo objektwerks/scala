b0d4ee0a28885d2e7a0c071f62c95ed890a5ccb2
objektwerks <objektwerks@gmail.com>
2014-07-29 12:16:00 -0400

    enhanced covariance test

--- src/test/scala/types/TypesTest.scala
+++ src/test/scala/types/TypesTest.scala
@@ -3,23 +3,22 @@ package types
 import org.scalatest.FunSuite
 
 class TypesTest extends FunSuite {
-  class Animal()
-  class Dinosaur(sound: String) extends Animal { override def toString = sound }
-  class Bird(sound: String) extends Dinosaur(sound) { override def toString = sound }
-
   test("generic function") {
     def getMiddle[A](a: Array[A]): A = a(a.length / 2)
     assert(getMiddle(Array("a", "b", "c")) == "b")
   }
 
   test("covariance") {
+    trait Animal
+    class Dinosaur(sound: String) extends Animal { override def toString = sound }
+    class Bird(sound: String) extends Dinosaur(sound) { override def toString = sound }
     class Ping[+A] () {
       def test[B >: A] (b: B): String = b.toString
     }
     val ping = new Ping()
-    val dinosaur = new Dinosaur("screech")
-    val bird = new Bird("chirp chirp")
-    var animal = new Dinosaur("screech")
+    val dinosaur: Dinosaur = new Dinosaur("screech")
+    val bird: Bird = new Bird("chirp chirp")
+    var animal: Animal = new Dinosaur("screech")
     animal = new Bird("chirp chirp")
     assert(ping.test(dinosaur) == dinosaur.toString)
     assert(ping.test(bird) == bird.toString)
