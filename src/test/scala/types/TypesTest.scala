567f847da2ce0289b405470aca917a807537bc50
objektwerks <objektwerks@gmail.com>
2014-07-29 08:58:39 -0400

    added test

--- src/test/scala/types/TypesTest.scala
+++ src/test/scala/types/TypesTest.scala
@@ -3,18 +3,35 @@ package types
 import org.scalatest.FunSuite
 
 class TypesTest extends FunSuite {
+  trait Animal { def speak(): String }
+  case class Cat(sound: String) extends Animal { def speak(): String = sound }
+  case class Dog(sound: String) extends Animal { def speak(): String = sound }
+
   test("generic function") {
     def getMiddle[A](a: Array[A]): A = a(a.length / 2)
     assert(getMiddle(Array("a", "b", "c")) == "b")
   }
 
   test("covariance") {
+    class Container[+A] (val item: A)
+    val cat = Cat("meow")
+    val dog = Dog("woof")
+    val cats = new Container(cat)
+    val dogs = new Container(dog)
+    println(cats.item.speak())
+    println(dogs.item.speak())
   }
 
   test("contravariance") {
+    class Container[A] (val item: A) {
+
+    }
   }
 
   test("invariance") {
+    class Container[A] (val item: A) {
+
+    }
   }
 
   test("duck typing") {
