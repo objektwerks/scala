6bd565cdfe80bc5e5369b0459cf04ab99e07d458
objektwerks <objektwerks@gmail.com>
2014-08-04 14:09:09 -0400

    refactored test structure

--- src/test/scala/slick/SlickTest.scala
+++ src/test/scala/slick/SlickTest.scala
@@ -1,22 +1,41 @@
 package slick
 
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite}
 
 import scala.slick.driver.H2Driver.simple._
 
-class SlickTest extends FunSuite {
-  test("test") {
-    val users = TableQuery[Users]
-    val tasks = TableQuery[Tasks]
+class SlickTest extends FunSuite with BeforeAndAfter {
+  implicit var session: Session = _
+  implicit val users: TableQuery[Users] = TableQuery[Users]
+  implicit val tasks: TableQuery[Tasks] = TableQuery[Tasks]
 
-    Database.forURL("jdbc:h2:mem:test", driver = "org.h2.Driver") withSession { implicit session =>
-      (users.ddl ++ tasks.ddl).create
+  before {
+    session = Database.forURL("jdbc:h2:mem:test", driver = "org.h2.Driver").createSession()
+  }
+
+  after {
+    session.close()
+  }
+
+  test("slick") {
+    (users.ddl ++ tasks.ddl).create
+    insert()
+    query()
+  }
+
+  def insert() = {
+    session.withTransaction {
       val userId = (users returning users.map(_.id)) += User(None, "Fred")
       assert(userId == 1)
       val taskId = (tasks returning tasks.map(_.id)) += Task(None, userId, "Take a nap!")
       assert(taskId == 1)
       println(users.list)
       println(tasks.list)
+    }
+  }
+
+  def query() = {
+    session.withTransaction {
       val userTasks = for {
         (u, t) <- users innerJoin tasks
       } yield (u.name, t.task)
