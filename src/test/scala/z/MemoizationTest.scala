902ab0bf3d32854c31a07590da2de11e48847b30
objektwerks <objektwerks@gmail.com>
2014-08-08 13:33:21 -0400

    refactored magic numbers into val and var; added assert

--- src/test/scala/z/MemoizationTest.scala
+++ src/test/scala/z/MemoizationTest.scala
@@ -6,9 +6,12 @@ import org.scalatest.FunSuite
 import scalaz._
 
 class MemoizationTest extends FunSuite {
-  val fibonacciNumber: Long = 39
+  val fibonacciSeed: Long = 39
+  val fibonacciNumber: BigInt = BigInt(63245986)
+  var fibonacciNumberComputedCount: Int = 0
 
   def computeFibonacciNumber(n: Long): BigInt = {
+    fibonacciNumberComputedCount = fibonacciNumberComputedCount + 1
     val f = Fibonacci.number(n)
     println(s"Computed fibonacci number from $n to $f")
     f
@@ -18,7 +21,8 @@ class MemoizationTest extends FunSuite {
     val memo = Memo.immutableHashMapMemo {
       n: Long => computeFibonacciNumber(n)
     }
-    assert(memo(fibonacciNumber) == BigInt(63245986))  // Computed and cached.
-    assert(memo(fibonacciNumber) == BigInt(63245986)) // Cached value returned.
+    assert(memo(fibonacciSeed) == fibonacciNumber)  // Computed and cached.
+    assert(memo(fibonacciSeed) == fibonacciNumber) // Cached value returned.
+    assert(fibonacciNumberComputedCount == 1)
   }
 }
