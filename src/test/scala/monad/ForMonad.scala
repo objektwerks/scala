fa34f1b86fafc9d1e0b89d888607b61f136d6174
objektwerks <objektwerks@gmail.com>
2014-08-13 14:12:34 -0400

    enhanced for monad tests

--- src/test/scala/monad/ForMonad.scala
+++ src/test/scala/monad/ForMonad.scala
@@ -3,12 +3,30 @@ package monad
 import org.scalatest.FunSuite
 
 class ForMonad extends FunSuite {
-  test("for comprehension vs map") {
-    val list = List(1, 2)
-    val forList = for (i <- list) yield i * 2
-    val mapList = list map {i => i * 2}
-    assert (forList == List(2, 4))
-    assert (mapList == List(2, 4))
+  test("for > map") {
+    val xs = List(1, 2)
+    val forList = for (x <- xs) yield x * 2
+    val mapList = xs map { x => x * 2 }
+    assert(forList == List(2, 4))
+    assert(mapList == List(2, 4))
   }
 
+  test("for > flatmap > map") {
+    val xs = List(2, 4)
+    val ys = List (3, 5)
+    val forList = for (x <- xs; y <- ys) yield x * y
+    val mapList = xs flatMap { e => ys map { o => e * o }}
+    assert (forList == List (2 * 3, 2 * 5, 4 * 3, 4 * 5))
+    assert (mapList == List (2 * 3, 2 * 5, 4 * 3, 4 * 5))
+  }
+
+  test("for > flatmap > flatmap > map") {
+    val xs = List(2, 4)
+    val ys = List (3, 5)
+    val zs = List(1, 6)
+    val forList = for (x <- xs; y <- ys; z <- zs) yield x * y * z
+    val mapList = xs flatMap { x => ys flatMap { y => { zs map { z => x * y * z }}}}
+    assert(forList == List(6, 36, 10, 60, 12, 72, 20, 120))
+    assert(mapList == List(6, 36, 10, 60, 12, 72, 20, 120))
+  }
 }
