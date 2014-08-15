6ad12158227b160cf1473509bfd9520a31b32aee
objektwerks <objektwerks@gmail.com>
2014-08-15 13:51:03 -0400

    shortened profile case class for a more readable test

--- src/test/scala/z/ValidationTest.scala
+++ src/test/scala/z/ValidationTest.scala
@@ -5,36 +5,27 @@ import org.scalatest.FunSuite
 import scalaz.Scalaz._
 import scalaz._
 
-case class Profile(name: String, address: String, email: String)
+case class Profile(name: String, email: String)
 
 object Profile {
   def validateName(name: String): Validation[String, String] = {
-    if (name.isEmpty) "Please, enter your name.".failure
-    else name.success
-  }
-
-  def validateAddress(address: String): Validation[String, String] = {
-    if (address.isEmpty) "Please, enter your address.".failure
-    else address.success
+    if (name.isEmpty) "Please, enter your name.".failure else name.success
   }
 
   def validateEmail(email: String): Validation[String, String] = {
-    if (email.isEmpty) "Please, enter your email.".failure
-    else email.success
+    if (email.isEmpty) "Please, enter your email.".failure else email.success
   }
 
   def validateProfile(profile: Profile) : ValidationNel[String, Profile] = {
-    (validateName(profile.name).toValidationNel
-      |@| validateAddress(profile.address).toValidationNel
-      |@| validateEmail(profile.email).toValidationNel) {
-      Profile(_, _, _)
+    (validateName(profile.name).toValidationNel |@| validateEmail(profile.email).toValidationNel) {
+      Profile(_, _)
     }
   }
 }
 
 class ValidationTest extends FunSuite {
   test("invalid profile") {
-    val profile = Profile("", "", "")
+    val profile = Profile("", "")
     val validation = Profile.validateProfile(profile)
     assert(validation.isFailure)
     println(validation)
@@ -42,7 +33,7 @@ class ValidationTest extends FunSuite {
   }
 
   test("valid profile") {
-    val profile = Profile("Barney Rebel", "33 Stone St., Bolder City", "barney.rebel@gmail.com")
+    val profile = Profile("Barney Rebel", "barney.rebel@gmail.com")
     val validation = Profile.validateProfile(profile)
     assert(validation.isSuccess)
     println(validation)
