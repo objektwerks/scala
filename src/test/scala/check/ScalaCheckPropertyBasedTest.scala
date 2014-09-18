a1e47dc22e44899436eb4df1e5656928a400cf86
objektwerks <objektwerks@gmail.com>
2014-09-18 14:17:27 -0400

    added and cleaned up a few scala check/test tests. pretty boring stuff, but useful

--- src/test/scala/check/ScalaCheckPropertyBasedTest.scala
+++ src/test/scala/check/ScalaCheckPropertyBasedTest.scala
@@ -1,11 +1,20 @@
 package check
 
 import org.scalacheck.Prop.forAll
+import org.scalacheck.{Gen, Prop}
 import org.scalatest.FunSuite
 
 class ScalaCheckPropertyBasedTest extends FunSuite {
-  test("boolean check") {
-    val propListSize = forAll { (xs: List[Int], ys: List[Int]) => xs.size + ys.size == (xs ::: ys).size }
+  test("check") {
+    val propListSize = forAll {
+      (xs: List[Int], ys: List[Int]) => xs.size + ys.size == (xs ::: ys).size
+    }
     propListSize.check
+  }
+
+  test("generator check") {
+    val smallInteger = Gen.choose(0, 100)
+    val propSmallInteger = Prop.forAll(smallInteger)(n => n >= 0 && n <= 100)
+    propSmallInteger.check
   }
 }
--- src/test/scala/check/ScalaTestPropertyBasedTest.scala
+++ src/test/scala/check/ScalaTestPropertyBasedTest.scala
@@ -7,13 +7,13 @@ import org.scalatest.prop.{Checkers, GeneratorDrivenPropertyChecks}
 class ScalaTestPropertyBasedTest extends FunSuite with Checkers with GeneratorDrivenPropertyChecks {
   implicit override val generatorDrivenConfig = PropertyCheckConfig(minSuccessful = 10, maxDiscarded = 100, minSize = 10, maxSize = 100, workers = 1)
 
-  test("checkers check style") {
+  test("check") {
     check((xs: List[Int], ys: List[Int]) => xs.size + ys.size == (xs ::: ys).size)
   }
 
-  test("generator driven property checks style") {
-    forAll { (n: Int) =>
-      whenever (n > 1) { n / 2 should be > 0 }
+  test("generator check") {
+    forAll {
+      (n: Int) => whenever (n > 1) { n / 2 should be > 0 }
     }
   }
 }
