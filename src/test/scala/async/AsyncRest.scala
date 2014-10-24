d85ab2b9ac29e7ac235f89c011f1bc8ca41dd094
objektwerks <objektwerks@gmail.com>
2014-10-24 16:02:48 -0400

    collapsed rest delegate into async rest

--- src/test/scala/async/AsyncRest.scala
+++ src/test/scala/async/AsyncRest.scala
@@ -1,12 +1,47 @@
 package async
 
-import scala.async.Async.{async, await}
-import scala.concurrent.Future
+import java.net.URL
+import java.nio.charset.StandardCharsets
 
-object AsyncRest extends RestDelegate {
+import org.json4s.DefaultFormats
+import org.json4s.jackson.JsonMethods._
+
+import scala.async.Async.{async, await}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.io.Source
+
+object AsyncRest {
+  private implicit lazy val formats = DefaultFormats
+  private val url = new URL("http://api.icndb.com/jokes/random/")
+  private val utf8 = StandardCharsets.UTF_8.name()
+
   def asyncJoke: Future[String] = async {
     val jsonFuture = getJson
     val json = await(jsonFuture)
     await(parseJson(json))
   }
+
+  private def getJson = Future {
+    Source.fromURL(url, utf8).mkString
+  }
+
+  private def parseJson(json: String) = Future {
+    val ast = parse(json)
+    (ast \ "value" \ "joke").extract[String]
+  }
 }
+
+/*
+{ "type": "success",
+  "value": { "id": 111,
+             "joke": "Chuck Norris has a deep and abiding respect for human life... unless it gets in his way.",
+             "categories": []
+           }
+}
+
+JObject(List((type, JString(success)),
+             (value, JObject(List((id, JInt(174)),
+                                 (joke, JString(Chuck Norris cannot love, he can only not kill.)),
+                                 (categories, JArray(List())))))))
+ */
