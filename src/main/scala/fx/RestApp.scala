6170f10a5a3828c8faed2760381aa497d72e9e29
objektwerks <objektwerks@gmail.com>
2014-11-01 17:32:13 -0400

    added rest app to use async rest

---
+++ src/main/scala/fx/RestApp.scala
@@ -0,0 +1,70 @@
+package fx
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scalafx.Includes._
+import scalafx.application.{JFXApp, Platform}
+import scalafx.event.ActionEvent
+import scalafx.geometry.{Insets, Pos}
+import scalafx.scene.Scene
+import scalafx.scene.control._
+import scalafx.scene.layout.{BorderPane, HBox, VBox}
+
+import rest.AsyncRest
+
+object RestApp extends JFXApp {
+  private val jokeLabel = new Label {
+    text = "Joke:"
+  }
+
+  private val jokeText = new TextArea() {
+    wrapText = true
+  }
+
+  private val indicator = new ProgressBar {
+    progress = 0.0
+  }
+
+  private val jokeButton = new Button {
+    text = "New Joke"
+    defaultButton = true
+    onAction = (e: ActionEvent) => {
+      Platform.runLater(indicator.progress = 0.99)
+      val future = AsyncRest.asyncJoke
+      future.map {
+        text => Platform.runLater(jokeText.text = text)
+        Platform.runLater(indicator.progress = 0.0)
+      }
+    }
+  }
+
+  private val jokePane = new BorderPane {
+    center = new VBox {
+      maxHeight = 140
+      spacing = 6
+      padding = Insets(6)
+      content = List(jokeLabel, jokeText, jokeButton)
+    }
+    bottom = new HBox {
+      alignment = Pos.BaselineRight
+      spacing = 6
+      padding = Insets(6)
+      content = indicator
+    }
+  }
+
+  private val appPane = new VBox {
+    maxWidth = 400
+    maxHeight = 400
+    spacing = 6
+    padding = Insets(6)
+    content = List(jokePane)
+  }
+
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Chuck Norris Jokes"
+    scene = new Scene {
+      root = appPane
+    }
+  }
+}
