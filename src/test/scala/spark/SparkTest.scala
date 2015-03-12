7a4b6fa7763b865d00a417e5e560263f6d78b09d
objektwerks <objektwerks@gmail.com>
2015-03-12 17:33:41 -0400

    playing with the spark rdd api

--- src/test/scala/spark/SparkTest.scala
+++ src/test/scala/spark/SparkTest.scala
@@ -7,7 +7,12 @@ class SparkTest extends FunSuite {
   test("spark") {
     val conf = new SparkConf().setMaster("local").setAppName("sparky")
     val context = new SparkContext(conf)
-    val rdd = context.makeRDD(Array("a", "b", "c"))
-    assert(rdd.count() == 3)
+    val rdd = context.makeRDD(Array(1, 2, 3))
+    assert(rdd.count == 3)
+    assert(rdd.min == 1)
+    assert(rdd.max == 3)
+    assert(rdd.filter(_ % 2 == 0).first == 2)
+    assert(rdd.filter(_ % 2 != 0).first == 1)
+    assert(rdd.reduce(_ + _) == 6)
   }
 }
