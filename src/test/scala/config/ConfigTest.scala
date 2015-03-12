fc0cc21785784cd3f00064112b5962c2174167b5
objektwerks <objektwerks@gmail.com>
2015-03-12 16:20:42 -0400

    added typesafe config file and test

---
+++ src/test/scala/config/ConfigTest.scala
@@ -0,0 +1,13 @@
+package config
+
+import com.typesafe.config.ConfigFactory
+import org.scalatest.FunSuite
+
+class ConfigTest extends FunSuite {
+  test("load") {
+    val config = ConfigFactory.load()
+    assert(config.getString("app.name") == "config")
+    assert(config.getString("app.db.url") == "jdbc:h2:mem:test;INIT=RUNSCRIPT FROM 'classpath:ddl.sql")
+    assert(config.getString("app.db.driver") == "org.h2.Driver")
+  }
+}
