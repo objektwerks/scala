16cc27cfa89acabbd258fe9a09ebdf581ae84bf1
objektwerks <objektwerks@gmail.com>
2015-03-13 08:05:19 -0400

    add int test to config test

--- src/test/scala/config/ConfigTest.scala
+++ src/test/scala/config/ConfigTest.scala
@@ -9,5 +9,7 @@ class ConfigTest extends FunSuite {
     assert(config.getString("app.name") == "config")
     assert(config.getString("app.db.url") == "jdbc:h2:mem:test;INIT=RUNSCRIPT FROM 'classpath:ddl.sql")
     assert(config.getString("app.db.driver") == "org.h2.Driver")
+    assert(config.getString("app.http.host") == "localhost")
+    assert(config.getInt("app.http.port") == 9999)
   }
 }
