5c0d1da3402f253d9ae9bac660ef936d0903ae15
objektwerks <objektwerks@gmail.com>
2015-03-26 15:34:05 -0400

    simplified monoid test, and added scaladoc

--- src/test/scala/theory/MonoidTest.scala
+++ src/test/scala/theory/MonoidTest.scala
@@ -2,36 +2,26 @@ package theory
 
 import org.scalatest.FunSuite
 
-/** See Functional Programming in Scala, Part 3, Chapter 10 */
+/** See Functional Programming in Scala, Part 3, Chapter 10.
+  *
+  * A monoid is a type together with a binary operation (op)
+  * over that type, satisfying associativity and having an
+  * identity element (zero).
+  * */
 trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
 }
 
-object Monoid {
-  def add = new Monoid[Int] {
-    def op(a1: Int, a2: Int): Int = a1 + a2
-    def zero: Int = 0
-  }
-
-  def multiply = new Monoid[Int] {
-    def op(a1: Int, a2: Int): Int = a1 * a2
-    def zero: Int = 0
-  }
-
-  def concat = new Monoid[String] {
-    def op(a1: String, a2: String): String = a1 + a2
-    def zero: String = ""
-  }
+class Adder extends Monoid[Int] {
+  def op(a1: Int, a2: Int): Int = a1 + a2
+  def zero: Int = 0
 }
 
 class MonoidTest extends FunSuite {
   test("monoid") {
-    assert(Monoid.add.op(1, 1) == 2)
-    assert(Monoid.add.zero == 0)
-    assert(Monoid.multiply.op(1, 1) == 1)
-    assert(Monoid.multiply.zero == 0)
-    assert(Monoid.concat.op("a", "b") == "ab")
-    assert(Monoid.concat.zero == "")
+    val adder = new Adder
+    assert(adder.op(1, 1) == 2)
+    assert(adder.zero == 0)
   }
 }
