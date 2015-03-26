ddf6424b2c4871b88a283b4b32f44a24e1f693b9
objektwerks <objektwerks@gmail.com>
2015-03-26 15:44:21 -0400

    implemented the monoid laws

--- src/test/scala/theory/MonoidTest.scala
+++ src/test/scala/theory/MonoidTest.scala
@@ -4,9 +4,9 @@ import org.scalatest.FunSuite
 
 /** See Functional Programming in Scala, Part 3, Chapter 10.
   *
-  * A monoid is a type together with a binary operation (op)
-  * over that type, satisfying associativity and having an
-  * identity element (zero).
+  * A monoid is a (1) type together with a binary operation (op)
+  * over that type, satisfying (2) associativity and having an
+  * (3) identity element (zero).
   * */
 trait Monoid[A] {
   def op(a1: A, a2: A): A
@@ -14,8 +14,13 @@ trait Monoid[A] {
 }
 
 class Adder extends Monoid[Int] {
-  def op(a1: Int, a2: Int): Int = a1 + a2
+  def op(x: Int, y: Int): Int = x + y
   def zero: Int = 0
+  def isValid(x: Int, y: Int, z: Int): Boolean = {
+    val associative = op(op(x, y), z) == op(x, op(y, z))
+    val identity = op(zero, x) == x
+    associative && identity
+  }
 }
 
 class MonoidTest extends FunSuite {
@@ -23,5 +28,6 @@ class MonoidTest extends FunSuite {
     val adder = new Adder
     assert(adder.op(1, 1) == 2)
     assert(adder.zero == 0)
+    assert(adder.isValid(1, 2, 3))
   }
 }
