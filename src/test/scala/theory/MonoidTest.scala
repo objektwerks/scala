986aa989a8302946f4ab7c84a043d7f321a982aa
objektwerks <objektwerks@gmail.com>
2015-03-26 15:25:27 -0400

    enhanced monoid test

--- src/test/scala/theory/MonoidTest.scala
+++ src/test/scala/theory/MonoidTest.scala
@@ -2,20 +2,36 @@ package theory
 
 import org.scalatest.FunSuite
 
+/** See Functional Programming in Scala, Part 3, Chapter 10 */
 trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
 }
 
-class Adder extends Monoid[Int] {
-  def op(a1: Int, a2: Int) = a1 + a2
-  def zero: Int = 0
+object Monoid {
+  def add = new Monoid[Int] {
+    def op(a1: Int, a2: Int): Int = a1 + a2
+    def zero: Int = 0
+  }
+
+  def multiply = new Monoid[Int] {
+    def op(a1: Int, a2: Int): Int = a1 * a2
+    def zero: Int = 0
+  }
+
+  def concat = new Monoid[String] {
+    def op(a1: String, a2: String): String = a1 + a2
+    def zero: String = ""
+  }
 }
 
 class MonoidTest extends FunSuite {
   test("monoid") {
-    val adder = new Adder
-    assert(adder.op(1, 1) == 2)
-    assert(adder.zero == 0)
+    assert(Monoid.add.op(1, 1) == 2)
+    assert(Monoid.add.zero == 0)
+    assert(Monoid.multiply.op(1, 1) == 1)
+    assert(Monoid.multiply.zero == 0)
+    assert(Monoid.concat.op("a", "b") == "ab")
+    assert(Monoid.concat.zero == "")
   }
 }
