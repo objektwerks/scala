5a07680fe7a6df09f3570ea8e883cdeae9b403be
objektwerks <objektwerks@gmail.com>
2015-03-28 15:44:11 -0400

    removed code

--- src/test/scala/theory/Monad.scala
+++ src/test/scala/theory/Monad.scala
@@ -1,9 +1,7 @@
 package theory
 
-trait Monad[F[_]] {
+trait Monad[F[_]] extends Functor[F] {
   def unit[A](a: => A): F[A]
 
-  def flatMap[A, B](a: F[A])(f: A => F[B]): F[B]
-
-  def map[A, B](a: F[A])(f: A => B): F[B]
+  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
 }
