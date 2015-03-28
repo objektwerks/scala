1e7a3133cf6b698c3d7347baa6ce56bc5c8f99de
objektwerks <objektwerks@gmail.com>
2015-03-28 14:08:34 -0400

    reformatted code

--- src/test/scala/theory/MonadTest.scala
+++ src/test/scala/theory/MonadTest.scala
@@ -3,13 +3,17 @@ package theory
 import org.scalatest.FunSuite
 
 trait Functor[F[_]] {
+  def apply[A](x: A): F[A]
+
   def map[A, B](fa: F[A])(f: A => B): F[B]
 }
 
 trait Monad[F[_]] {
   def unit[A](a: => A): F[A]
-  def map[A, B](fa: F[A])(f: A => B): F[B]
+
   def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  def map[A, B](fa: F[A])(f: A => B): F[B]
 }
 
 class MonadTest extends FunSuite {
--- src/test/scala/theory/MonoidTest.scala
+++ src/test/scala/theory/MonoidTest.scala
@@ -2,21 +2,19 @@ package theory
 
 import org.scalatest.FunSuite
 
-/** See Functional Programming in Scala, Part 3, Chapter 10.
-  *
-  * A monoid is a (1) type together with a binary operation (op)
-  * over that type, satisfying (2) associativity and having an
-  * (3) identity element (zero).
-  * */
 trait Monoid[A] {
   def op(a1: A, a2: A): A
+
   def zero: A
 }
 
 object Adder extends Monoid[Int] {
-  def op(x: Int, y: Int): Int = x + y
-  def zero: Int = 0
+  override def op(x: Int, y: Int): Int = x + y
+
+  override def zero: Int = 0
+
   def fold(xs: List[Int]): Int = xs.fold(zero)(op)
+
   def isValid(x: Int, y: Int, z: Int): Boolean = {
     val associative = op(op(x, y), z) == op(x, op(y, z))
     val identity = op(zero, x) == x
