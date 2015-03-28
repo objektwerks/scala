437ea9cc8a8675b67762a84038bd9cb1246c1860
objektwerks <objektwerks@gmail.com>
2015-03-28 18:03:19 -0400

    edited monad trait

--- src/test/scala/theory/Monad.scala
+++ src/test/scala/theory/Monad.scala
@@ -1,22 +1,9 @@
 package theory
 
 trait Monad[M[_]] {
-  def unit[A](a: A): M[A]
+  def unit[A](a: => A): M[A]
 
-  def map[A, B](f: A => B): M[A] => M[B]
+  def map[A, B](a: M[A])(f: A => B): M[B]
 
-  def flatten[A](m: M[M[A]]): M[A]
-
-  def flatMap[A, B](m: M[A])(f: A => M[B]): M[B]
-
-}
-
-object ListMonad extends Monad[List] {
-  override def unit[A](a: A) = a :: Nil
-
-  override def map[A,B](f: A => B) = (xs: List[A]) => xs map f
-
-  override def flatten[A](xs: List[List[A]]) = xs flatten
-
-  override def flatMap[A, B](xs: List[A])(f: (A) => List[B]): List[B] = xs map f flatten
+  def flatMap[A, B](a: M[A])(f: A => M[B]): M[B]
 }
