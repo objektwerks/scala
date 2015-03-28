0de3a7e2f5cc7670d80e460dffceb466caf769ef
objektwerks <objektwerks@gmail.com>
2015-03-28 12:15:45 -0400

    refactored fold test

--- src/test/scala/theory/MonoidTest.scala
+++ src/test/scala/theory/MonoidTest.scala
@@ -13,9 +13,10 @@ trait Monoid[A] {
   def zero: A
 }
 
-class Adder extends Monoid[Int] {
+object Adder extends Monoid[Int] {
   def op(x: Int, y: Int): Int = x + y
   def zero: Int = 0
+  def fold(xs: List[Int]): Int = xs.fold(zero)(op)
   def isValid(x: Int, y: Int, z: Int): Boolean = {
     val associative = op(op(x, y), z) == op(x, op(y, z))
     val identity = op(zero, x) == x
@@ -24,18 +25,10 @@ class Adder extends Monoid[Int] {
 }
 
 class MonoidTest extends FunSuite {
-  val adder = new Adder
-
   test("monoid") {
-    assert(adder.op(1, 1) == 2)
-    assert(adder.zero == 0)
-    assert(adder.isValid(1, 2, 3))
-  }
-
-  test("fold") {
-    val nums = List(1, 2, 3)
-    assert(6 == nums.foldLeft(adder.zero)(adder.op))
-    assert(6 == nums.foldRight(adder.zero)(adder.op))
-    assert(6 == nums.fold(adder.zero)(adder.op))
+    assert(Adder.op(1, 1) == 2)
+    assert(Adder.zero == 0)
+    assert(Adder.fold(List(1, 2, 3)) == 6)
+    assert(Adder.isValid(1, 2, 3))
   }
 }
