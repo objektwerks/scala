14d05a86744b954ffedbb50ea1a05e80f9401273
objektwerks <objektwerks@gmail.com>
2015-03-28 15:14:44 -0400

    refactored test

--- src/test/scala/theory/FunctorTest.scala
+++ src/test/scala/theory/FunctorTest.scala
@@ -4,8 +4,9 @@ import org.scalatest.FunSuite
 
 class FunctorTest extends FunSuite {
   test("functor") {
-    val nums = List(1, 2, 3)
-    val strings = ToListOfStrings.map(nums)(_.toString)
-    assert(strings == List("1", "2", "3"))
+    val listOfNumbers = List(1, 2, 3)
+    val listOfStrings = ToListOfStrings.map(listOfNumbers)(_.toString)
+    val expectedMorphism = List("1", "2", "3")
+    assert(listOfStrings == expectedMorphism)
   }
 }
