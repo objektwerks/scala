f64dd02fc1afbb45c3a1355932877d29bcddd8dd
objektwerks <objektwerks@gmail.com>
2015-03-28 17:07:43 -0400

    renamed monoid zero to id

--- src/test/scala/theory/Monoid.scala
+++ src/test/scala/theory/Monoid.scala
@@ -3,19 +3,19 @@ package theory
 trait Monoid[A] {
   def op(a1: A, a2: A): A
 
-  def zero: A
+  def id: A
 }
 
 object Adder extends Monoid[Int] {
   override def op(x: Int, y: Int): Int = x + y
 
-  override def zero: Int = 0
+  override def id: Int = 0
 
-  def fold(xs: List[Int]): Int = xs.fold(zero)(op)
+  def fold(xs: List[Int]): Int = xs.fold(id)(op)
 
   def isValid(x: Int, y: Int, z: Int): Boolean = {
     val associative = op(op(x, y), z) == op(x, op(y, z))
-    val identity = op(zero, x) == x
+    val identity = op(id, x) == x
     associative && identity
   }
 }
--- src/test/scala/theory/MonoidTest.scala
+++ src/test/scala/theory/MonoidTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.FunSuite
 class MonoidTest extends FunSuite {
   test("monoid") {
     assert(Adder.op(1, 1) == 2)
-    assert(Adder.zero == 0)
+    assert(Adder.id == 0)
     assert(Adder.fold(List(1, 2, 3)) == 6)
     assert(Adder.isValid(1, 2, 3))
   }
