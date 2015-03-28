1edef92a1e56f092a3aeba928f4fa4a7317b89d0
objektwerks <objektwerks@gmail.com>
2015-03-28 19:22:30 -0400

    implemented bento as monad

--- src/test/scala/theory/Monad.scala
+++ src/test/scala/theory/Monad.scala
@@ -7,3 +7,11 @@ trait Monad[M[_]] {
 
   def flatMap[A, B](a: M[A])(f: A => M[B]): M[B]
 }
+
+class Bento extends Monad[List[String]] {
+  override def unit[A](a: => A): List[A] = a :: Nil
+
+  override def map[A, B](a: List[A])(f: (A) => B): List[B] = a map f
+
+  override def flatMap[A, B](a: List[A])(f: (A) => List[B]): List[B] = a flatmap f
+}
