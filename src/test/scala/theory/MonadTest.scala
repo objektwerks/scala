2904bbc655b377ed738eec250e13aaabc12d2a96
objektwerks <objektwerks@gmail.com>
2015-03-29 16:31:21 -0400

    implemented option monad

--- src/test/scala/theory/MonadTest.scala
+++ src/test/scala/theory/MonadTest.scala
@@ -3,6 +3,22 @@ package theory
 import org.scalatest.FunSuite
 
 class MonadTest extends FunSuite {
+  test("option monad") {
+    val optionMonad = new Monad[Option] {
+      override def unit[A](a: => A): Option[A] = Some(a)
+      override def map[A, B](a: Option[A])(f: (A) => B): Option[B] = a map f
+      override def flatMap[A, B](a: Option[A])(f: (A) => Option[B]): Option[B] = a flatMap f
+    }
+    val option: Option[Int] = optionMonad.unit(1)
+    val mappedOption: Option[Int] = option.map(i => i * 3)
+    val flatMappedOption: Option[Int] = option.flatMap(i => Some(i))
+    assert(option.get == 1)
+    assert(mappedOption.get == 3)
+    assert(flatMappedOption.get == 1)
+    assert(option != mappedOption)
+    assert(option == flatMappedOption)
+  }
+
   test("identity monad") {
     val identity: Identity[Int] = Identity(1)
     val mappedIdentity: Identity[Int] = identity.map(i => i * 3)
