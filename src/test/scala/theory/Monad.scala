0c52c9086228d2a9f0116e935f2ccc286baeaf3e
objektwerks <objektwerks@gmail.com>
2015-03-29 10:06:28 -0400

    implemented list monad and test

--- src/test/scala/theory/Monad.scala
+++ src/test/scala/theory/Monad.scala
@@ -8,10 +8,10 @@ trait Monad[M[_]] {
   def flatMap[A, B](a: M[A])(f: A => M[B]): M[B]
 }
 
-class Box extends Monad[List] {
-  override def unit[A](a: => A): List[A] = a :: Nil
-
-  override def map[A, B](a: List[A])(f: (A) => B): List[B] = a map f
+object ListMonad extends Monad[List] {
+  override def unit[A](a: => A): List[A] = List(a)
 
   override def flatMap[A, B](a: List[A])(f: (A) => List[B]): List[B] = a flatMap f
+
+  override def map[A, B](a: List[A])(f: (A) => B): List[B] = a map f
 }
--- src/test/scala/theory/MonadTest.scala
+++ src/test/scala/theory/MonadTest.scala
@@ -4,5 +4,17 @@ import org.scalatest.FunSuite
 
 class MonadTest extends FunSuite {
   test("monad") {
+    val list: List[String] = ListMonad.unit("a")
+    val mappedListToUpperCase: List[String] = list map(a => a.toUpperCase)
+    val flatMappedListToLowerCase: List[Char] = list flatMap(a => a.toLowerCase)
+    println(mappedListToUpperCase)
+    println(flatMappedListToLowerCase)
+    assert(list.length + mappedListToUpperCase.length + flatMappedListToLowerCase.length == 3)
+    val listOfList: List[List[String]] = ListMonad.unit("a" :: "b" :: "c" :: Nil)
+    val flatMappedListOfList = listOfList flatMap(as => as.map(a => a.toUpperCase))
+    println(listOfList)
+    println(flatMappedListOfList)
+    assert(listOfList.length == 1)
+    assert(flatMappedListOfList.length == 3)
   }
 }
