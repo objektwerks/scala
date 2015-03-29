1c9dd5772a1cc39137af7bb7d19dae2c6e44ed17
objektwerks <objektwerks@gmail.com>
2015-03-29 12:23:13 -0400

    removed list monad and test

--- src/test/scala/theory/Monad.scala
+++ src/test/scala/theory/Monad.scala
@@ -7,11 +7,3 @@ trait Monad[M[_]] {
 
   def flatMap[A, B](a: M[A])(f: A => M[B]): M[B]
 }
-
-object ListMonad extends Monad[List] {
-  override def unit[A](a: => A): List[A] = List(a)
-
-  override def flatMap[A, B](a: List[A])(f: (A) => List[B]): List[B] = a flatMap f
-
-  override def map[A, B](a: List[A])(f: (A) => B): List[B] = a map f
-}
