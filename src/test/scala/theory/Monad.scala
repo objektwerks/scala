11acdedf90c611479b3901c50022d789c026512a
objektwerks <objektwerks@gmail.com>
2015-03-31 12:31:38 -0400

    added compose and flatten to monad

--- src/test/scala/theory/Monad.scala
+++ src/test/scala/theory/Monad.scala
@@ -2,6 +2,10 @@ package theory
 
 trait Monad[F[_]] extends Functor[F]{
   def unit[A](a: => A): F[A]
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C
+
+  def flatten[A](f: F[F[A]]): F[A]
   
   def flatMap[A, B](a: F[A])(f: A => F[B]): F[B]
 }
