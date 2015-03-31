9bedc6cc891682dd5b2f229f904dd2963e5d043e
objektwerks <objektwerks@gmail.com>
2015-03-31 15:20:31 -0400

    moved all theory tests to category test

---
+++ src/test/scala/theory/CategoryTest.scala
@@ -0,0 +1,48 @@
+package theory
+
+import org.scalatest.FunSuite
+
+class CategoryTest extends FunSuite {
+  test("monoid") {
+    assert(Adder.op(1, 1) == 2)
+    assert(Adder.id == 0)
+    assert(Adder.fold(List(1, 2, 3)) == 6)
+    assert(Adder.isValid(1, 2, 3))
+  }
+
+  test("functor") {
+    val listOfNumbers = List(1, 2, 3)
+    val listOfStrings = ToListOfStrings.map(listOfNumbers)(_.toString)
+    val expectedMorphism = List("1", "2", "3")
+    assert(listOfStrings == expectedMorphism)
+  }
+
+  test("option monad") {
+    val optionMonad = new Monad[Option] {
+      override def unit[A](a: => A): Option[A] = Option(a)
+      override def flatten[A](a: Option[Option[A]]): Option[A] = a flatMap identity
+      override def map[A, B](a: Option[A])(f: (A) => B): Option[B] = a map f
+      override def flatMap[A, B](a: Option[A])(f: (A) => Option[B]): Option[B] = a flatMap f
+    }
+    val option: Option[Int] = optionMonad.unit(1)
+    val mappedOption: Option[Int] = option.map(i => i * 3)
+    val flattenedOption: Option[Int] = optionMonad.flatten(Option(option))
+    val flatMappedOption: Option[Int] = option.flatMap(i => Some(i))
+    assert(option.get == 1)
+    assert(flattenedOption.get == 1)
+    assert(mappedOption.get == 3)
+    assert(flatMappedOption.get == 1)
+    assert(option != mappedOption)
+    assert(option == flatMappedOption)
+  }
+
+  test("identity monad") {
+    val identity: Identity[Int] = Identity(1)
+    val mappedIdentity: Identity[Int] = identity.map(i => i * 3)
+    val flatMappedIdentity: Identity[Int] = identity.flatMap { i => Identity(i) }
+    assert(mappedIdentity.value == 3)
+    assert(flatMappedIdentity.value == 1)
+    assert(identity != mappedIdentity)
+    assert(identity == flatMappedIdentity)
+  }
+}
