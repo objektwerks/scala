094c2f02c70c01825b0beba013b31fcde27eedae
objektwerks <objektwerks@gmail.com>
2015-03-31 13:22:18 -0400

    removed compose from monad

--- src/test/scala/theory/Monad.scala
+++ src/test/scala/theory/Monad.scala
@@ -3,9 +3,7 @@ package theory
 trait Monad[F[_]] extends Functor[F]{
   def unit[A](a: => A): F[A]
 
-  def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C]
-
-  def flatten[A](f: F[F[A]]): F[A]
+  def flatten[A](a: F[F[A]]): F[A]
   
   def flatMap[A, B](a: F[A])(f: A => F[B]): F[B]
 }
--- src/test/scala/theory/MonadTest.scala
+++ src/test/scala/theory/MonadTest.scala
@@ -5,16 +5,17 @@ import org.scalatest.FunSuite
 class MonadTest extends FunSuite {
   test("option monad") {
     val optionMonad = new Monad[Option] {
-      override def unit[A](a: => A): Option[A] = Some(a)
-      override def compose[A, B, C](f: (A) => Option[B], g: (B) => Option[C]): (A) => Option[C] = ???
-      override def flatten[A](f: Option[Option[A]]): Option[A] = f flatten
+      override def unit[A](a: => A): Option[A] = Option(a)
+      override def flatten[A](a: Option[Option[A]]): Option[A] = a flatMap identity
       override def map[A, B](a: Option[A])(f: (A) => B): Option[B] = a map f
       override def flatMap[A, B](a: Option[A])(f: (A) => Option[B]): Option[B] = a flatMap f
     }
     val option: Option[Int] = optionMonad.unit(1)
     val mappedOption: Option[Int] = option.map(i => i * 3)
+    val flattenedOption: Option[Int] = optionMonad.flatten(Option(option))
     val flatMappedOption: Option[Int] = option.flatMap(i => Some(i))
     assert(option.get == 1)
+    assert(flattenedOption.get == 1)
     assert(mappedOption.get == 3)
     assert(flatMappedOption.get == 1)
     assert(option != mappedOption)
