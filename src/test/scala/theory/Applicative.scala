969168ec30bc3775cf442298ce7650ea3bfa96a9
objektwerks <objektwerks@gmail.com>
2015-03-31 12:19:25 -0400

    added map, map override and traverse

--- src/test/scala/theory/Applicative.scala
+++ src/test/scala/theory/Applicative.scala
@@ -1,5 +1,15 @@
 package theory
 
-trait Applicative[F[_]] extends Functor {
+trait Applicative[F[_]] extends Functor[F] {
   def unit[A](a: => A): F[A]
+
+  def map[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+
+  override def map[A, B](a: F[A])(f: A => B): F[B] = {
+    map(a, unit(()))((a, _) => f(a))
+  }
+
+  def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] = {
+    as.foldRight(unit(List[B]()))((a, fbs) => map(f(a), fbs)(_ :: _))
+  }
 }
