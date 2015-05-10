d1aa74e3f18259949a9be68fc7d143c876c1ae2e
objektwerks <objektwerks@gmail.com>
2015-05-10 07:30:44 -0400

    upgraded spark version; and enhanced test

--- build.sbt
+++ build.sbt
@@ -25,7 +25,7 @@ libraryDependencies ++= {
     "org.scalafx" % "scalafx_2.11" % "8.0.40-R8",
     "net.databinder.dispatch" % "dispatch-core_2.11" % "0.11.2",
     "com.h2database" % "h2" % "1.4.187",
-    "org.apache.spark" % "spark-core_2.11" % "1.3.0",
+    "org.apache.spark" % "spark-core_2.11" % "1.3.1",
     "org.json4s" % "json4s-jackson_2.11" % json4sVersion,
     "org.json4s" % "json4s-native_2.11" % json4sVersion,
     "io.spray" %% "spray-can" % sprayVersion,
--- src/test/scala/spark/SparkTest.scala
+++ src/test/scala/spark/SparkTest.scala
@@ -4,11 +4,13 @@ import org.apache.spark.{SparkContext, SparkConf}
 import org.scalatest.FunSuite
 
 class SparkTest extends FunSuite {
+  val conf = new SparkConf().setMaster("local").setAppName("sparky")
+  val context = new SparkContext(conf)
+
   test("spark") {
-    val conf = new SparkConf().setMaster("local").setAppName("sparky")
-    val context = new SparkContext(conf)
     val rdd = context.makeRDD(Array(1, 2, 3))
     assert(rdd.count == 3)
+    assert(rdd.first == 1)
     assert(rdd.min == 1)
     assert(rdd.max == 3)
     assert(rdd.filter(_ % 2 == 0).first == 2)
