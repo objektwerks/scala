6d4f96db944b9a1fa649495f0100342e78278c86
objektwerks <objektwerks@gmail.com>
2015-05-10 08:16:58 -0400

    added spark parallelize test

--- src/test/scala/spark/SparkTest.scala
+++ src/test/scala/spark/SparkTest.scala
@@ -7,7 +7,7 @@ class SparkTest extends FunSuite {
   val conf = new SparkConf().setMaster("local").setAppName("sparky")
   val context = new SparkContext(conf)
 
-  test("spark") {
+  test("core") {
     val rdd = context.makeRDD(Array(1, 2, 3))
     assert(rdd.count == 3)
     assert(rdd.first == 1)
@@ -16,5 +16,12 @@ class SparkTest extends FunSuite {
     assert(rdd.filter(_ % 2 == 0).first == 2)
     assert(rdd.filter(_ % 2 != 0).first == 1)
     assert(rdd.reduce(_ + _) == 6)
+  }
+
+  test("parallelize") {
+    val data = 1 to 1000000
+    val rdd = context.parallelize(data)
+    val result = rdd.filter(_ % 2 == 0).collect()
+    assert(result.length == 500000)
   }
 }
