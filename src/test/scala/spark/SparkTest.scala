51d36f65d66136803810daf81612becdb4bddb4e
objektwerks <objektwerks@gmail.com>
2015-05-13 18:07:13 -0400

    trying to fix logging to a file

--- src/test/scala/spark/SparkTest.scala
+++ src/test/scala/spark/SparkTest.scala
@@ -1,5 +1,6 @@
 package spark
 
+import org.apache.log4j.{Logger, Level}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
@@ -9,6 +10,9 @@ import scala.collection.mutable
 import scala.io.Source
 
 class SparkTest extends FunSuite with BeforeAndAfterAll {
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+
   val conf = new SparkConf().setMaster("local[2]").setAppName("sparky")
   val context = new SparkContext(conf)
   val streamingContext = new StreamingContext(context, Seconds(1))
