edceaea1a864707715e28fb9c42bfecebe322398
objektwerks <objektwerks@gmail.com>
2015-05-13 08:39:13 -0400

    enhanced actor test

--- src/test/scala/akka/ActorTest.scala
+++ src/test/scala/akka/ActorTest.scala
@@ -5,19 +5,22 @@ import java.util.concurrent.TimeUnit
 import akka.actor._
 import akka.pattern.ask
 import akka.util.Timeout
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 import scala.concurrent.ExecutionContext.Implicits.{global => ec}
 import scala.util.{Failure, Success}
 
 class Master extends Actor {
   println(s"Master created: $self")
+  private implicit val timeout = new Timeout(1, TimeUnit.SECONDS)
   val worker: ActorRef = context.actorOf(Props(new Worker()), name = "worker")
 
   def receive = {
     case Message(Tell, who, message) => println(s"\nMaster received $message from $who.")
+    case Message(TellWorker, who, message) => worker ! new Message(Tell, s"$who -> Master", message)
     case Message(Ask, who, message) => sender ! s"Master received $message from $who."
-    case Message(TellDelegate, who, message) => worker ! new Message(Tell, "Master", message)
+    case Message(AskWorker, who, message) => sender ! s"Master received $message from $who. Asked worker."
+                                              worker ? new Message(AskWorker, s"$who -> Master", message)
     case _ => println("Master received invalid message.")
   }
 }
@@ -26,39 +29,52 @@ class Worker extends Actor {
   println(s"Worker created: $self")
   def receive = {
     case Message(Tell, who, message) => println(s"Worker received $message from $who.")
+    case Message(AskWorker, who, message) => sender ! s"Worker received $message from $who."
     case _ => println("Worker received invalid message.")
   }
 }
 
 sealed trait Mode
 case object Tell extends Mode
-case object TellDelegate extends Mode
+case object TellWorker extends Mode
 case object Ask extends Mode
+case object AskWorker extends Mode
 case class Message(mode: Mode, who: String, message: String)
 
-class ActorTest extends FunSuite {
+class ActorTest extends FunSuite with BeforeAndAfterAll {
   private implicit val timeout = new Timeout(1, TimeUnit.SECONDS)
   private val system: ActorSystem = ActorSystem.create("system")
   private val master: ActorRef = system.actorOf(Props(new Master()), name = "master")
   println(s"Actor system created: $system")
 
-  test("async one way ! -> tell") {
-    master ! Message(Tell, "System", "an async one way ! -> tell message")
-    master ! Message(TellDelegate, "System", "an async one way ! -> tell message")
+  override protected def afterAll(): Unit = {
+    super.afterAll()
+    println(s"Actor system shutdown: $system")
+    system.shutdown()
+    system.awaitTermination()
   }
 
-  test("async two way ? -> ask") {
+  test("async one way ! -> tell master") {
+    master ! Message(Tell, "System", "an async one way ! -> tell message")
+  }
+
+  test("async one way ! -> tell worker via master") {
+    master ! Message(TellWorker, "System", "an async one way ! -> tell worker message")
+  }
+
+  test("async two way ? -> ask master") {
     val future = master ? Message(Ask, "System", "an async two way ? -> ask message")
     future onComplete {
-      case Success(result) => kill(result)
+      case Success(result) => println(result)
       case Failure(failure) => throw failure
     }
   }
 
-  private def kill(result: Any) {
-    println(result)
-    println(s"Actor system shutdown: $system")
-    system.shutdown()
-    system.awaitTermination()
+  test("async two way ? -> ask worker via master") {
+    val future = master ? Message(AskWorker, "System", "an async two way ? -> ask worker message")
+    future onComplete {
+      case Success(result) => println(result)
+      case Failure(failure) => throw failure
+    }
   }
 }
