42b1bf0bc6e6cfcf4c93db66f9f0451517a5c853
objektwerks <objektwerks@gmail.com>
2015-05-14 09:33:24 -0400

    refactored spark text test

--- src/test/scala/spark/SparkTest.scala
+++ src/test/scala/spark/SparkTest.scala
@@ -33,16 +33,20 @@ class SparkTest extends FunSuite with BeforeAndAfterAll {
     val rdd = context.textFile("license.mit").cache()
     val lineCount = rdd.count()
     assert(lineCount == 19)
-    assert(rdd.filter(_.contains("Permission")).count == 1)
+
+    val wordPermission = rdd.filter(_.contains("Permission")).count
+    assert(wordPermission == 1)
+
     val longestLine = rdd.map(l => l.length).reduce((a, b) => Math.max(a, b))
     assert(longestLine == 77)
-    val wordCountRdd = rdd.flatMap(l => l.split("\\P{L}+")).map(_.toLowerCase).map(w => (w, 1)).
-      reduceByKey(_ + _).map(p => p.swap).sortByKey(ascending = false, 1).cache()
+
+    val wordCountRdd = rdd.flatMap(l => l.split("\\P{L}+")).map(_.toLowerCase).map(w => (w, 1)).reduceByKey(_ + _).cache()
     val wordCount = wordCountRdd.count()
     assert(wordCount == 96)
-    val (count, word) = wordCountRdd.max
-    assert(count == 14)
-    assert(word == "the")
+
+    val maxWordCount = wordCountRdd.values.max()
+    val (word, count) = wordCountRdd.filter(_._2 == maxWordCount).first()
+    assert(word == "the" && count == 14)
   }
 
   test("parallelize") {
