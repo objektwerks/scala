79aa9c990a66dd82a43d99884a1cee5c3441ce45
objektwerks <objektwerks@gmail.com>
2015-05-18 07:22:19 -0400

    refactored spark test

--- src/test/scala/spark/SparkTest.scala
+++ src/test/scala/spark/SparkTest.scala
@@ -20,15 +20,26 @@ class SparkTest extends FunSuite with BeforeAndAfterAll {
     streamingContext.stop(stopSparkContext = true, stopGracefully = true)
   }
 
-  test("core") {
+  test("transformations") {
+    val rdd = context.makeRDD(Array(1, 2, 3))
+    assert(rdd.filter(_ % 2 == 0).first == 2)
+    assert(rdd.filter(_ % 2 != 0).first == 1)
+    assert(rdd.reduce(_ + _) == 6)
+  }
+
+  test("actions") {
     val rdd = context.makeRDD(Array(1, 2, 3))
     assert(rdd.count == 3)
     assert(rdd.first == 1)
     assert(rdd.min == 1)
     assert(rdd.max == 3)
-    assert(rdd.filter(_ % 2 == 0).first == 2)
-    assert(rdd.filter(_ % 2 != 0).first == 1)
-    assert(rdd.reduce(_ + _) == 6)
+  }
+
+  test("parallelize") {
+    val data = 1 to 1000000
+    val rdd = context.parallelize(data)
+    val result = rdd.filter(_ % 2 == 0).collect()
+    assert(result.length == 500000)
   }
 
   test("text") {
@@ -49,13 +60,6 @@ class SparkTest extends FunSuite with BeforeAndAfterAll {
     val maxWordCount = wordCountRdd.values.max()
     val (word, count) = wordCountRdd.filter(_._2 == maxWordCount).first()
     assert(word == "the" && count == 14)
-  }
-
-  test("parallelize") {
-    val data = 1 to 1000000
-    val rdd = context.parallelize(data)
-    val result = rdd.filter(_ % 2 == 0).collect()
-    assert(result.length == 500000)
   }
 
   test("streaming") {
