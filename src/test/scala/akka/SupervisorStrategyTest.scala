5d19c25766904d02beae74d7b1e6b50ea8efb02b
objektwerks <objektwerks@gmail.com>
2015-06-11 08:55:27 -0400

    added watch and watcher actor

--- src/test/scala/akka/SupervisorStrategyTest.scala
+++ src/test/scala/akka/SupervisorStrategyTest.scala
@@ -2,18 +2,19 @@ package akka
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.SupervisorStrategy.{Resume, Restart, Escalate}
+import akka.actor.SupervisorStrategy.{Stop, Resume, Restart, Escalate}
 import akka.actor._
 import akka.util.Timeout
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.{global => ec}
 
 trait Task
+case object Play extends Task
 case object CleanRoom extends Task
 case object CleanWindows extends Task
 case object CleanBathroom extends Task
 case object StandInCorner extends Task
-case object Play extends Task
 
 class CleanRoomException(cause: String) extends Exception(cause)
 class CleanWindowsException(cause: String) extends Exception(cause)
@@ -30,15 +31,15 @@ class Nanny extends Actor {
     case _: CleanRoomException => Restart
     case _: CleanWindowsException => Restart
     case _: CleanBathroomException => Restart
-    case _: StandInCornerException => Escalate
+    case _: StandInCornerException => Stop
   }
 
   def receive = {
+    case Play => child ! Play
     case CleanRoom => child ! CleanRoom
     case CleanWindows => child ! CleanWindows
     case CleanBathroom => child ! CleanBathroom
     case StandInCorner => child ! StandInCorner
-    case Play => child ! Play
     case _ => println("Nanny received an invalid message.")
   }
 }
@@ -47,11 +48,11 @@ class Child extends Actor {
   println(s"Child created: $self")
 
   def receive = {
+    case Play => println("Child happily wishes to play!")
     case CleanRoom => throw new CleanRoomException("Child refuses to clean room!")
     case CleanWindows => throw new CleanWindowsException("Child refuses to clean windows!")
     case CleanBathroom => throw new CleanBathroomException("Child refuses to clean bathroom!")
     case StandInCorner => throw new StandInCornerException("Child refuses to stand in corner!")
-    case Play => println("Child happily wishes to play!")
     case _ => println("Child received an invalid message.")
   }
 
@@ -76,10 +77,21 @@ class Child extends Actor {
   }
 }
 
+class Watcher extends Actor {
+  private implicit val timeout = new Timeout(1, TimeUnit.SECONDS)
+  private val futureChild = context.system.actorSelection("/user/nanny/*").resolveOne()
+  futureChild onSuccess { case child => context.watch(child)}
+
+  def receive = {
+    case Terminated(child) => println(s"Watcher terminated event: ${child.path.name} TERMINATED!")
+  }
+}
+
 class SupervisorStrategyTest extends FunSuite with BeforeAndAfterAll {
   private implicit val timeout = new Timeout(1, TimeUnit.SECONDS)
   private val system: ActorSystem = ActorSystem.create("funky")
   private val nanny: ActorRef = system.actorOf(Props[Nanny], name = "nanny")
+  system.actorOf(Props[Watcher], name = "watcher")
   println(s"Actor system created: $system")
 
   override protected def afterAll(): Unit = {
@@ -90,6 +102,8 @@ class SupervisorStrategyTest extends FunSuite with BeforeAndAfterAll {
   }
 
   test("nanny ! child") {
+    nanny ! Play
+    Thread.sleep(1000)
     nanny ! CleanRoom
     Thread.sleep(1000)
     nanny ! CleanWindows
@@ -97,7 +111,5 @@ class SupervisorStrategyTest extends FunSuite with BeforeAndAfterAll {
     nanny ! CleanBathroom
     Thread.sleep(1000)
     nanny ! StandInCorner
-    Thread.sleep(1000)
-    nanny ! Play
   }
 }
