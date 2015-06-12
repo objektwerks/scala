9fb8c976fef6e703237f79f016c67101837b1c2a
objektwerks <objektwerks@gmail.com>
2015-06-12 14:03:36 -0400

    added value class test

--- src/test/scala/classes/ClassesTest.scala
+++ src/test/scala/classes/ClassesTest.scala
@@ -2,6 +2,14 @@ package classes
 
 import org.scalatest.FunSuite
 
+case class Meter(value: Double) extends AnyVal {
+  def toFeet: Foot = Foot(value * 0.3048)
+}
+
+case class Foot(value: Double) extends AnyVal {
+  def toMeter: Meter = Meter(value / 0.3048)
+}
+
 class ClassesTest extends FunSuite {
   abstract class Animal {
     def sound: String
@@ -23,10 +31,15 @@ class ClassesTest extends FunSuite {
     def openCages: Set[Animal] = Set(new Tiger, new Shark, new Bear)
   }
 
-  test("animals") {
+  test("classes") {
     val animals = ZooKeeper.openCages
     for (animal <- animals) {
       assert(animal.sound.length > 0)
     }
+  }
+
+  test("value classes") {
+    assert(Meter(3.0).toFeet == Foot(0.9144000000000001))
+    assert(Foot(3.0).toMeter == Meter(9.84251968503937))
   }
 }
