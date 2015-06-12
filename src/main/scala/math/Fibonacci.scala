1aa0aa5f92b831e2fa1625c8e312739168c90d5f
objektwerks <objektwerks@gmail.com>
2015-06-12 08:00:47 -0400

    reorder methods

--- src/main/scala/math/Fibonacci.scala
+++ src/main/scala/math/Fibonacci.scala
@@ -3,15 +3,6 @@ package math
 import scala.annotation.tailrec
 
 object Fibonacci {
-  def tailrec(n: Long): BigInt = {
-    @tailrec
-    def loop(n: Long, a: Long, b: Long): BigInt = n match {
-      case 0 => a
-      case _ => loop(n - 1, b, a + b)
-    }
-    loop(n, 0, 1)
-  }
-
   def naiveRecursive(n : Long) : BigInt = n match {
     case 0 | 1 => n
     case _ => naiveRecursive(n - 1) + naiveRecursive(n - 2)
@@ -20,6 +11,15 @@ object Fibonacci {
   def tailRecursive(n: Long, a: Long, b: Long): BigInt = n match {
     case 0 => a
     case _ => tailRecursive(n - 1, b, a + b)
+  }
+
+  def tailrec(n: Long): BigInt = {
+    @tailrec
+    def loop(n: Long, a: Long, b: Long): BigInt = n match {
+      case 0 => a
+      case _ => loop(n - 1, b, a + b)
+    }
+    loop(n, 0, 1)
   }
 
   def sequence(a: Int = 0, b: Int = 1): List[Int] = {
