128084f0b3e5533e6932e96fdb14dbe550ebc2ad
objektwerks <objektwerks@gmail.com>
2015-06-13 19:22:02 -0400

    added akka router test

---
+++ src/test/scala/akka/RouterTest.scala
@@ -0,0 +1,56 @@
+package akka
+
+import java.time.LocalTime
+import java.util.concurrent.TimeUnit
+
+import akka.actor._
+import akka.pattern._
+import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
+import akka.util.Timeout
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+import scala.concurrent.ExecutionContext.Implicits.{global => ec}
+
+class Clock extends Actor {
+  var router = {
+    val routees = Vector.fill(3) {
+      val t = context.actorOf(Props[Time])
+      context watch t
+      ActorRefRoutee(t)
+    }
+    Router(RoundRobinRoutingLogic(), routees)
+  }
+
+  def receive = {
+    case m: String => router.route(m, sender())
+    case Terminated(a) =>
+      router = router.removeRoutee(a)
+      val t = context.actorOf(Props[Time])
+      context watch t
+      router = router.addRoutee(t)
+  }
+}
+
+class Time extends Actor {
+  def receive = {
+    case m: String => sender.tell(s"$m" + LocalTime.now().toString, context.parent)
+  }
+}
+
+class RouterTest extends FunSuite with BeforeAndAfterAll {
+  private implicit val timeout = new Timeout(1, TimeUnit.SECONDS)
+  private val system: ActorSystem = ActorSystem.create("funky")
+  private val clock: ActorRef = system.actorOf(Props[Clock], name = "clock")
+
+  override protected def afterAll(): Unit = {
+    super.afterAll
+    system.shutdown
+    system.awaitTermination
+  }
+
+  test("router") {
+    clock ? "time a: " onSuccess { case m: String => println(m) }
+    clock ? "time b: " onSuccess { case m: String => println(m) }
+    clock ? "time c: " onSuccess { case m: String => println(m) }
+  }
+}
