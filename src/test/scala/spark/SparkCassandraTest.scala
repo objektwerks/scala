9e155a6d68c708e5945f8e7b952c19bea78a44c3
objektwerks <objektwerks@gmail.com>
2015-06-20 09:57:31 -0400

    embedded cassandra doesn't work

--- src/test/scala/spark/SparkCassandraTest.scala
+++ src/test/scala/spark/SparkCassandraTest.scala
@@ -1,10 +1,10 @@
 package spark
 
+import com.datastax.spark.connector._
 import com.datastax.spark.connector.cql.CassandraConnector
 import com.datastax.spark.connector.embedded.EmbeddedCassandra
-import com.datastax.spark.connector._
 import com.typesafe.config.ConfigFactory
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 /*
@@ -18,22 +18,24 @@ import org.scalatest.{BeforeAndAfterAll, FunSuite}
   (ssl_storage_port,7100)
   (storage_port,7000)
   (seeds,127.0.0.1)
-
-  Occasionally a NoHostAvailableException occurs. Running Cassandra from the console mysteriously resovles the error.
  */
-class SparkCassandraTest extends FunSuite with BeforeAndAfterAll {
+class SparkCassandraTest extends FunSuite with BeforeAndAfterAll with EmbeddedCassandra {
+  System.setProperty("CASSANDRA_HOME", "/Users/javawerks/workspace/cassandra")
+  useCassandraConfig(Seq("/Users/javawerks/workspace/cassandra/conf/cassandra.yaml"), forceReload = true)
   val props = ConfigFactory.load("spark.properties")
-  val cassandra = EmbeddedCassandra
   val conf = new SparkConf().setMaster(props.getString("spark.master"))
                             .setAppName(props.getString("spark.app.name"))
                             .set("spark.cassandra.connection.host", props.getString("spark.cassandra.connection.host"))
   val context = new SparkContext(conf)
-  cassandra.getProps(0).foreach(println)
+  val connector = CassandraConnector(conf)
+  EmbeddedCassandra.getProps(0).foreach(println)
 
+  override def clearCache(): Unit = {
+  }
 
   override protected def beforeAll(): Unit = {
     super.beforeAll()
-    CassandraConnector(conf).withSessionDo { session =>
+    connector.withSessionDo { session =>
       session.execute("CREATE KEYSPACE test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };")
       session.execute("CREATE TABLE test.kv(key text PRIMARY KEY, value int);")
       session.execute("INSERT INTO test.kv(key, value) VALUES ('k1', 1);")
@@ -44,7 +46,7 @@ class SparkCassandraTest extends FunSuite with BeforeAndAfterAll {
 
   override protected def afterAll(): Unit = {
     super.afterAll
-    CassandraConnector(conf).withSessionDo { session =>
+    connector.withSessionDo { session =>
       session.execute("DROP KEYSPACE test;")
     }
     context.stop
