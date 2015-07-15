7d7756a56d1fc6fe7cf6118cd89009a89476dd4d
objektwerks <objektwerks@gmail.com>
2015-07-15 06:29:18 -0400

    added scala enum and test

--- src/test/scala/enums/EnumTest.scala
+++ src/test/scala/enums/EnumTest.scala
@@ -2,10 +2,25 @@ package enums
 
 import org.scalatest.FunSuite
 
+object WeekDay extends Enumeration {
+  type WeekDay = Value
+  val Mon, Tue, Wed, Thu, Fri, Sat, Sun = Value
+}
+
 class EnumTest extends FunSuite {
   test("java enum") {
-    Lights.red == Lights.red
-    Lights.yellow == Lights.yellow
-    Lights.green == Lights.green
+    assert(Lights.red == Lights.red)
+    assert(Lights.yellow == Lights.yellow)
+    assert(Lights.green == Lights.green)
+  }
+
+  test("scala enum") {
+    assert(WeekDay.values.contains(WeekDay.Mon))
+    assert(WeekDay.values.contains(WeekDay.Tue))
+    assert(WeekDay.values.contains(WeekDay.Wed))
+    assert(WeekDay.values.contains(WeekDay.Thu))
+    assert(WeekDay.values.contains(WeekDay.Fri))
+    assert(WeekDay.values.contains(WeekDay.Sat))
+    assert(WeekDay.values.contains(WeekDay.Sun))
   }
 }
