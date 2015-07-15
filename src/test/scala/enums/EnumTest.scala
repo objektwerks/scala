3a209ab66510581dc837e1ba6c10f4ce4eae3f2a
objektwerks <objektwerks@gmail.com>
2015-07-15 06:10:39 -0400

    renamed test

--- src/test/scala/enums/EnumTest.scala
+++ src/test/scala/enums/EnumTest.scala
@@ -3,7 +3,7 @@ package enums
 import org.scalatest.FunSuite
 
 class EnumTest extends FunSuite {
-  test("enum") {
+  test("java enum") {
     Lights.red == Lights.red
     Lights.yellow == Lights.yellow
     Lights.green == Lights.green
