4a48363f8271c2a059059d0f5047a67ff3e611ee
objektwerks <objektwerks@gmail.com>
2015-07-23 15:26:49 -0400

    refactored validation test

--- src/test/scala/z/ValidationTest.scala
+++ src/test/scala/z/ValidationTest.scala
@@ -5,24 +5,17 @@ import org.scalatest.FunSuite
 import scalaz.Scalaz._
 import scalaz._
 
-case class Profile(name: String, email: String)
-
-object Profile {
-  def validateName(name: String): Validation[String, String] = {
+case class Profile(name: String, email: String) {
+  private def validateName(name: String): Validation[String, String] = {
     if (name.isEmpty) "Please, enter your name.".failure else name.success
   }
 
-  def validateEmail(email: String): Validation[String, String] = {
+  private def validateEmail(email: String): Validation[String, String] = {
     if (email.isEmpty) "Please, enter your email.".failure else email.success
   }
 
-  def validate(profile: Profile): ValidationNel[String, Profile] = {
-    (validateName(profile.name).toValidationNel |@| validateEmail(profile.email).toValidationNel) {
-      Profile(_, _)
-    }
-  }
-
-  def isValid(validation: ValidationNel[String, Profile]): Boolean = {
+  def isValid: Boolean = {
+    val validation = (validateName(name).toValidationNel |@| validateEmail(email).toValidationNel) { Profile(_, _) }
     validation match {
       case Success(s) => true
       case Failure(f) => false
@@ -33,13 +26,11 @@ object Profile {
 class ValidationTest extends FunSuite {
   test("valid profile") {
     val profile = Profile("Barney Rebel", "barney.rebel@gmail.com")
-    val validationNel = Profile.validate(profile)
-    assert(Profile.isValid(validationNel))
+    assert(profile.isValid)
   }
 
   test("invalid profile") {
-    val invalidProfile = Profile("", "")
-    val validationNel = Profile.validate(invalidProfile)
-    assert(!Profile.isValid(validationNel))
+    val profile = Profile("", "")
+    assert(!profile.isValid)
   }
 }
