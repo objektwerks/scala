3c181425b2bf34c5d42e8d3f35494b0cac9e2ecd
objektwerks <objektwerks@gmail.com>
2015-08-29 19:30:46 -0400

    added queue and stack tests

--- src/test/scala/collection/MutableTest.scala
+++ src/test/scala/collection/MutableTest.scala
@@ -5,6 +5,18 @@ import org.scalatest.FunSuite
 import scala.collection.mutable._
 
 class MutableTest extends FunSuite {
+  test("queue") {
+    val queue = Queue(1, 2, 3)
+    queue enqueue  4
+    assert(4 == queue.last)
+  }
+
+  test("stack") {
+    val stack = Stack(1, 2, 3)
+    stack push 4
+    assert(4 == stack.pop)
+  }
+
   test("array buffer") {
     var buffer = ArrayBuffer(1, 2, 3)
     assert((buffer += 4) == ArrayBuffer(1, 2, 3, 4))
