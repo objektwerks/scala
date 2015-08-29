3951c590fba59e0e51c79c68c95c8e3924082486
objektwerks <objektwerks@gmail.com>
2015-08-29 19:35:13 -0400

    corrected mutable import issue

--- src/test/scala/collection/MutableTest.scala
+++ src/test/scala/collection/MutableTest.scala
@@ -2,17 +2,18 @@ package collection
 
 import org.scalatest.FunSuite
 
-import scala.collection.mutable._
+import scala.collection.mutable
+import scala.collection.mutable.{ListBuffer, ArrayBuffer}
 
 class MutableTest extends FunSuite {
   test("queue") {
-    val queue = Queue(1, 2, 3)
+    val queue = mutable.Queue(1, 2, 3)
     queue enqueue  4
     assert(4 == queue.last)
   }
 
   test("stack") {
-    val stack = Stack(1, 2, 3)
+    val stack = mutable.Stack(1, 2, 3)
     stack push 4
     assert(4 == stack.pop)
   }
@@ -30,7 +31,7 @@ class MutableTest extends FunSuite {
   }
 
   test("list map") {
-    var map = Map(1 -> 1, 2 -> 2, 3 -> 3)
+    var map = mutable.Map(1 -> 1, 2 -> 2, 3 -> 3)
     assert((map += 4 -> 4) == Map(1 -> 1, 2 -> 2, 3 -> 3, 4 -> 4))
     assert((map -= 4) == Map(1 -> 1, 2 -> 2, 3 -> 3))
   }
