1659d4202f71cf88f04f193d12a2e4d5bf0c2bf4
objektwerks <objektwerks@gmail.com>
2015-08-29 19:37:05 -0400

    corrected, once again, mutable import issue

--- src/test/scala/collection/MutableTest.scala
+++ src/test/scala/collection/MutableTest.scala
@@ -3,7 +3,6 @@ package collection
 import org.scalatest.FunSuite
 
 import scala.collection.mutable
-import scala.collection.mutable.{ListBuffer, ArrayBuffer}
 
 class MutableTest extends FunSuite {
   test("queue") {
@@ -19,15 +18,15 @@ class MutableTest extends FunSuite {
   }
 
   test("array buffer") {
-    var buffer = ArrayBuffer(1, 2, 3)
-    assert((buffer += 4) == ArrayBuffer(1, 2, 3, 4))
-    assert((buffer -= 4) == ArrayBuffer(1, 2, 3))
+    var buffer = mutable.ArrayBuffer(1, 2, 3)
+    assert((buffer += 4) == mutable.ArrayBuffer(1, 2, 3, 4))
+    assert((buffer -= 4) == mutable.ArrayBuffer(1, 2, 3))
   }
 
   test("list buffer") {
-    var buffer = ListBuffer(1, 2, 3)
-    assert((buffer += 4) == ListBuffer(1, 2, 3, 4))
-    assert((buffer -= 4) == ListBuffer(1, 2, 3))
+    var buffer = mutable.ListBuffer(1, 2, 3)
+    assert((buffer += 4) == mutable.ListBuffer(1, 2, 3, 4))
+    assert((buffer -= 4) == mutable.ListBuffer(1, 2, 3))
   }
 
   test("list map") {
