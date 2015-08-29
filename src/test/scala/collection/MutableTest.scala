86edd4d2e0383458d81e8a48a8fb783254be3487
objektwerks <objektwerks@gmail.com>
2015-08-29 18:38:45 -0400

    removed map explicit type info despite intellij warnings

--- src/test/scala/collection/MutableTest.scala
+++ src/test/scala/collection/MutableTest.scala
@@ -18,8 +18,8 @@ class MutableTest extends FunSuite {
   }
 
   test("list map") {
-    var map = Map[Int, Int](1 -> 1, 2 -> 2, 3 -> 3)
-    assert((map += 4 -> 4) == Map[Int, Int](1 -> 1, 2 -> 2, 3 -> 3, 4 -> 4))
-    assert((map -= 4) == Map[Int, Int](1 -> 1, 2 -> 2, 3 -> 3))
+    var map = Map(1 -> 1, 2 -> 2, 3 -> 3)
+    assert((map += 4 -> 4) == Map(1 -> 1, 2 -> 2, 3 -> 3, 4 -> 4))
+    assert((map -= 4) == Map(1 -> 1, 2 -> 2, 3 -> 3))
   }
 }
