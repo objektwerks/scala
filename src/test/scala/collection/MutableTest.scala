fb1f3c18038d5acf8c254b59b0b8b5cd5e6900f6
objektwerks <objektwerks@gmail.com>
2015-08-29 17:14:04 -0400

    added list buffer test

--- src/test/scala/collection/MutableTest.scala
+++ src/test/scala/collection/MutableTest.scala
@@ -2,12 +2,18 @@ package collection
 
 import org.scalatest.FunSuite
 
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ListBuffer, ArrayBuffer}
 
 class MutableTest extends FunSuite {
   test("array buffer") {
     var buffer = ArrayBuffer(1, 2, 3)
     assert((buffer += 4) == ArrayBuffer(1, 2, 3, 4))
     assert((buffer -= 4) == ArrayBuffer(1, 2, 3))
+  }
+
+  test("list buffer") {
+    var buffer = ListBuffer(1, 2, 3)
+    assert((buffer += 4) == ListBuffer(1, 2, 3, 4))
+    assert((buffer -= 4) == ListBuffer(1, 2, 3))
   }
 }
