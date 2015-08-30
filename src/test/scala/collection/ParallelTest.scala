ea6ff4cbf2f4380c3ee977d22897eab7bc66f5e8
objektwerks <objektwerks@gmail.com>
2015-08-30 08:10:38 -0400

    changed test names

--- src/test/scala/collection/ParallelTest.scala
+++ src/test/scala/collection/ParallelTest.scala
@@ -6,22 +6,22 @@ import scala.collection.parallel.ParSet
 import scala.collection.parallel.immutable.{ParMap, ParSeq, ParRange}
 
 class ParallelTest extends FunSuite {
-  test("set sum") {
+  test("set") {
     val set = ParSet(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21)
     assert(set.sum == 231)
   }
 
-  test("map sum") {
+  test("map") {
     val map = ParMap(1 -> 1, 2 -> 2, 3 -> 3, 4 -> 4, 5 -> 5, 6 -> 6, 7 -> 7, 8 -> 8)
     assert(map.values.sum == 36)
   }
 
-  test("seq sum") {
+  test("seq") {
     val set = ParSeq(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21)
     assert(set.sum == 231)
   }
 
-  test("range sum") {
+  test("range") {
     val range = ParRange(1, 21, 1, inclusive = true)
     assert(range.sum == 231)
   }
