57bfe40a5f576689dbeff06fcd5031b6963bbfc1
objektwerks <objektwerks@gmail.com>
2015-08-30 07:55:22 -0400

    enhanced all mutable tests

--- src/test/scala/collection/MutableTest.scala
+++ src/test/scala/collection/MutableTest.scala
@@ -6,34 +6,45 @@ import scala.collection.mutable
 
 class MutableTest extends FunSuite {
   test("fifo queue") {
-    val queue = mutable.Queue(1, 2, 3)
-    queue enqueue  4
-    assert(4 == queue.last)
+    val queue = mutable.Queue(1, 2)
+    queue enqueue  3
+    assert(3 == queue.last)
     assert(queue.dequeue() == 1)
+    assert(queue.dequeue() == 2)
+    assert(queue.dequeue() == 3)
+    assert(queue.isEmpty)
   }
 
   test("lifo stack") {
-    val stack = mutable.Stack(3, 2, 1)
-    stack push 4
-    assert(4 == stack.pop)
+    val stack = mutable.Stack(2, 1)
+    stack push 3
     assert(3 == stack.pop)
+    assert(2 == stack.pop)
+    assert(1 == stack.pop)
+    assert(stack.isEmpty)
   }
 
   test("array buffer") {
-    var buffer = mutable.ArrayBuffer(1, 2, 3)
-    assert((buffer += 4) == mutable.ArrayBuffer(1, 2, 3, 4))
-    assert((buffer -= 4) == mutable.ArrayBuffer(1, 2, 3))
+    var buffer = mutable.ArrayBuffer(1, 2)
+    assert((buffer += 3) == mutable.ArrayBuffer(1, 2, 3))
+    assert((buffer -= 3) == mutable.ArrayBuffer(1, 2))
+    assert((buffer -= 2) == mutable.ArrayBuffer(1))
+    assert((buffer -= 1) == mutable.ArrayBuffer())
   }
 
   test("list buffer") {
-    var buffer = mutable.ListBuffer(1, 2, 3)
-    assert((buffer += 4) == mutable.ListBuffer(1, 2, 3, 4))
-    assert((buffer -= 4) == mutable.ListBuffer(1, 2, 3))
+    var buffer = mutable.ListBuffer(1, 2)
+    assert((buffer += 3) == mutable.ListBuffer(1, 2, 3))
+    assert((buffer -= 3) == mutable.ListBuffer(1, 2))
+    assert((buffer -= 2) == mutable.ListBuffer(1))
+    assert((buffer -= 1) == mutable.ListBuffer())
   }
 
   test("list map") {
-    var map = mutable.Map(1 -> 1, 2 -> 2, 3 -> 3)
-    assert((map += 4 -> 4) == Map(1 -> 1, 2 -> 2, 3 -> 3, 4 -> 4))
-    assert((map -= 4) == Map(1 -> 1, 2 -> 2, 3 -> 3))
+    var map = mutable.Map(1 -> 1, 2 -> 2)
+    assert((map += 3 -> 3) == Map(1 -> 1, 2 -> 2, 3 -> 3))
+    assert((map -= 3) == Map(1 -> 1, 2 -> 2))
+    assert((map -= 2) == Map(1 -> 1))
+    assert((map -= 1) == Map())
   }
 }
