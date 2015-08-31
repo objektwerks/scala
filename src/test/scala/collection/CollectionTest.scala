07070c45155e080fede7483570f628410f392c02
objektwerks <objektwerks@gmail.com>
2015-08-31 14:55:04 -0400

    reorganized list test

--- src/test/scala/collection/CollectionTest.scala
+++ src/test/scala/collection/CollectionTest.scala
@@ -9,6 +9,7 @@ class CollectionTest extends FunSuite {
   test("list") {
     def toList(v: Int) = List(v - 1, v, v + 1)
     val list = List(1, 2, 3)
+
     assert(list == 1 :: 2 :: 3 :: Nil)
     assert(list == List(1) ::: List(2, 3))
     assert(list == 1 :: List(2, 3))
@@ -18,11 +19,15 @@ class CollectionTest extends FunSuite {
     assert(list == List(1) ++: List(2, 3))
     assert((0 /: list)(_ + _) == 6)
     assert(6 == (list :\ 0)(_ + _))
+
+    assert(list == List(1, 1, 2, 2, 3, 3).distinct)
     assert(list == (List(1) union List(2, 3)))
     assert(list == (List(-2, -1, 0, 1, 2, 3) intersect List(1, 2, 3, 4, 5, 6)))
+
     assert(list.size == 3)
     assert(list.nonEmpty)
     assert(List().isEmpty)
+
     assert(list.head == 1)
     assert(list.headOption.get == 1)
     assert(list.tail == List(2, 3))
@@ -33,68 +38,88 @@ class CollectionTest extends FunSuite {
     assert(list.lastOption.get == 3)
     assert(list.lastIndexOfSlice(List(3)) == 2)
     assert(list.lastIndexWhere(_ > 2) == 2)
+
     assert(list.collect { case i if i % 2 == 0 => i } == List(2))
     assert(list.collectFirst { case i if i % 2 == 0 => i } == Some(2))
     assert(list.contains(1))
+    assert(list.count(_ > 0) == 3)
+
     assert((List(1, 2) diff List(2, 3)) == List(1))
     assert((List(2, 3) diff List(1, 2)) == List(3))
-    assert(list == List(1, 1, 2, 2, 3, 3).distinct)
+
     assert((list drop 1) == List(2, 3))
     assert(list.dropWhile(_ < 2) == List(2, 3))
     assert(list.dropRight(1) == List(1, 2))
+
     assert((list take 2) == List(1, 2))
     assert(list.takeWhile(_ < 3) == List(1, 2))
     assert(list.takeRight(1) == List(3))
+
     assert(list.min == 1)
     assert(list.minBy(_ * 2) == 1)
     assert(list.max == 3)
     assert(list.maxBy(_ * 2) == 3)
     assert(list.aggregate(0)(_ + _, _ + _) == 6)
-    assert(list.count(_ > 0) == 3)
+
     assert(list.filter(_ > 1) == List(2, 3))
     assert(list.filter(_ > 1).map(_ * 2) == List(4, 6))
     assert(list.filterNot(_ > 1) == List(1))
     assert(list.find(_ > 2).get == 3)
+
     assert(List(List(1), List(2), List(3)).flatten == list)
     assert(List(Some(1), None, Some(3), None).flatten == List(1, 3))
+
     assert(list.map(_ * 2) == List(2, 4, 6))
     assert(List("abc").map(_.toUpperCase) == List("ABC"))
+    assert(list.map(i => toList(i)) == List(List(0, 1, 2), List(1, 2, 3), List(2, 3, 4)))
+
     assert(list.flatMap(i => List(i * 2)) == List(2, 4, 6))
     assert(List("abc").flatMap(_.toUpperCase) == List('A', 'B', 'C'))
-    assert(list.map(i => toList(i)) == List(List(0, 1, 2), List(1, 2, 3), List(2, 3, 4)))
     assert(list.flatMap(i => toList(i)) == List(0, 1, 2, 1, 2, 3, 2, 3, 4))
+
     assert(list.fold(0)(_ + _) == 6)
     assert(list.foldLeft(0)(_ + _) == 6)
     assert(list.foldRight(0)(_ + _) == 6)
+
     assert(list.forall(_ > 0))
     list foreach { i => assert(i > 0) }
+
     assert(list.groupBy(_ % 2 == 0) == Map(false -> List(1, 3), true -> List(2)))
     assert(list.grouped(1).toList == List(List(1), List(2), List(3)))
+
     assert(list.mkString(", ") == "1, 2, 3")
+
     assert((List[Int](2), List[Int](1, 3)) == list.partition(_ % 2 == 0))
     assert(list.product == 6)
+
     assert(list.reduce(_ + _) == 6)
     assert(list.reduceLeft(_ + _) == 6)
     assert(list.reduceLeftOption(_ + _).get == 6)
     assert(list.reduceRight(_ + _) == 6)
     assert(list.reduceRightOption(_ + _).get == 6)
     assert(list == List(3, 2, 1).reverse)
+
     assert(list sameElements List(1, 2, 3))
     assert(list.segmentLength(_ > 0, 0) == 3)
+
     assert(list == List(3, 2, 1).sortBy(i => i))
     assert(list == List(3, 2, 1).sorted)
     assert(List(1, 2, 3).sortWith(_ > _) == List(3, 2, 1))
     assert(List(3, 2, 1).sortWith(_ < _) == List(1, 2, 3))
-    assert(list.sum == 6)
+
     assert(list.scan(0)(_ + _) == List(0, 1, 3, 6))
     assert(list.scanLeft(0)(_ + _) == List(0, 1, 3, 6))
     assert(list.scanRight(0)(_ + _) == List(6, 5, 3, 0))
+
     assert(list.slice(0, 2) == List(1, 2))
     assert(List(List(1), List(2), List(3)) == list.sliding(1).toList)
     assert((List[Int](1), List[Int](2, 3)) == list.span(_ < 2))
     assert((List[Int](1, 2), List[Int](3)) == list.splitAt(2))
+    assert(list.sum == 6)
+
     assert(List(1, 2, 1) == list.updated(index = 2, elem = 1))
     assert(List(2, 4, 6) == list.withFilter(_ > 0).map(_ * 2))
+
     assert((List[Int](1, 3),List[Int](2, 4)) == List((1, 2), (3, 4)).unzip)
     assert(List((1,3), (2,4)) == (List(1, 2) zip List(3, 4)))
     assert(List((1,3), (2,4), (3,5)) == List(1, 2, 3).zipAll(List(3, 4, 5), 0, 1))
