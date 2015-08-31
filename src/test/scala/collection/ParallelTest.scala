1e1e62dd556137d0ca98dd8f84b5c50c22f17bbd
objektwerks <objektwerks@gmail.com>
2015-08-31 08:27:21 -0400

    increased parallel seed to 1,000,000

--- src/test/scala/collection/ParallelTest.scala
+++ src/test/scala/collection/ParallelTest.scala
@@ -7,23 +7,23 @@ import scala.collection.parallel.immutable.{ParMap, ParSeq, ParRange}
 
 class ParallelTest extends FunSuite {
   test("set") {
-    val set = ParSet(1 to 21:_*)
-    assert(set.sum == 231)
+    val set = ParSet(1 to 1000000:_*)
+    assert(set.sum == 1784293664)
   }
 
   test("map") {
-    val m = for (i <- 1 to 21) yield (i , i)
+    val m = for (i <- 1 to 1000000) yield (i , i)
     val map = ParMap(m:_*)
-    assert(map.values.sum == 231)
+    assert(map.values.sum == 1784293664)
   }
 
   test("seq") {
-    val seq = ParSeq(1 to 21:_*)
-    assert(seq.sum == 231)
+    val seq = ParSeq(1 to 1000000:_*)
+    assert(seq.sum == 1784293664)
   }
 
   test("range") {
-    val range = ParRange(1, 21, 1, inclusive = true)
-    assert(range.sum == 231)
+    val range = ParRange(1, 1000000, 1, inclusive = true)
+    assert(range.sum == 1784293664)
   }
 }
