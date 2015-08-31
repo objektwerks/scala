65b9322ced926300020e642772ebb9a7a9f3a593
objektwerks <objektwerks@gmail.com>
2015-08-31 13:08:07 -0400

    removed immutable test

--- src/test/scala/collection/CollectionTest.scala
+++ src/test/scala/collection/CollectionTest.scala
@@ -10,6 +10,14 @@ class CollectionTest extends FunSuite {
 
   test("list") {
     val list = List(1, 2, 3)
+    assert(list == 1 :: 2 :: 3 :: Nil)
+    assert(list == List(1) ::: List(2, 3))
+    assert(list == 1 :: List(2, 3))
+    assert(list == 1 +: List(2, 3))
+    assert(list == List(1, 2) :+ 3)
+    assert(list == List(1) ++ List(2, 3))
+    assert(list == List(1) ++: List(2, 3))
+    assert(list == (List(1) union List(2, 3)))
     assert(list sameElements List(1, 2, 3))
     assert(list.size == 3)
     assert(list.nonEmpty)
@@ -77,6 +85,52 @@ class CollectionTest extends FunSuite {
     assert((List[Int](1, 2), List[Int](3)) == list.splitAt(2))
     assert((List[Int](1, 3),List[Int](2, 4)) == List((1, 2), (3, 4)).unzip)
     assert(List((1,3), (2,4)) == (List(1, 2) zip List(3, 4)))
+  }
+
+  test("set") {
+    val set = Set(1, 2)
+    assert(set == Set(1) ++ Set(2))
+    assert(set == Set(1) + 2)
+    assert(set == Set(1, 2, 3) - 3)
+    assert(set.contains(1))
+  }
+
+  test("map") {
+    val map = Map(1 -> 1, 2 -> 2)
+    assert(map == Map(1 -> 1) ++ Map(2 -> 2))
+    assert(map == Map(1 -> 1) + (2 -> 2))
+    assert(map == Map(1 -> 1, 2 -> 2, 3 -> 3) - 3)
+    assert(map.get(1).get == 1)
+  }
+
+  test("vector") {
+    val vector = Vector(1, 2)
+    assert(vector == 1 +: Vector(2))
+    assert(vector == Vector(1) :+ 2)
+    assert(vector == Vector(1) ++ Vector(2))
+    assert(vector == Vector(1) ++: Vector(2))
+    assert(vector.contains(1))
+  }
+
+  test("array") {
+    val array = Array(1, 2)
+    assert(array(0) == 1)
+    assert(array.reverse === Array(2, 1))
+    assert(array === Array(1) ++ Array(2))
+    assert(array === 1 +: Array(2))
+    assert(array === Array(1) :+ 2)
+  }
+
+  test("stream") {
+    val numberOfEvens = (1 to 100).toStream.count(_ % 2 == 0)
+    assert(numberOfEvens == 50)
+  }
+
+  test("tuple") {
+    val (first, last) = ("fred", "flintstone")
+    assert(first == "fred" && last == "flintstone")
+    val (city, state) = "barney" -> "rebel"
+    assert(city == "barney" && state == "rebel")
   }
 
   test("fifo queue") {
