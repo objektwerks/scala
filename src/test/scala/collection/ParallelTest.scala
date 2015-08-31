211e89a209ebe8e52fdb52506915ee9805642ff2
objektwerks <objektwerks@gmail.com>
2015-08-31 12:47:07 -0400

    renamed test function

--- src/test/scala/collection/ParallelTest.scala
+++ src/test/scala/collection/ParallelTest.scala
@@ -6,23 +6,23 @@ import scala.collection.parallel.ParSet
 import scala.collection.parallel.immutable.{ParMap, ParSeq, ParRange}
 
 class ParallelTest extends FunSuite {
-  test("set") {
+  test("par set") {
     val set = ParSet(1 to 1000000:_*)
     assert(set.sum == 1784293664)
   }
 
-  test("map") {
+  test("par map") {
     val m = for (i <- 1 to 1000000) yield (i , i)
     val map = ParMap(m:_*)
     assert(map.values.sum == 1784293664)
   }
 
-  test("seq") {
+  test("par seq") {
     val seq = ParSeq(1 to 1000000:_*)
     assert(seq.sum == 1784293664)
   }
 
-  test("range") {
+  test("par range") {
     val range = ParRange(1, 1000000, 1, inclusive = true)
     assert(range.sum == 1784293664)
   }
