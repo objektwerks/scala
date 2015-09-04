26e4004263037791d9164416ce36d3ba07117718
objektwerks <objektwerks@gmail.com>
2015-09-04 13:59:15 -0400

    cleaned up classes tests

--- src/test/scala/classes/CaseClassesTest.scala
+++ src/test/scala/classes/CaseClassesTest.scala
@@ -2,27 +2,35 @@ package classes
 
 import org.scalatest.FunSuite
 
+trait Animal {
+  def sound: String
+}
+
+case class Tiger(speach: String) extends Animal {
+  def sound: String = speach
+}
+
+case class Shark(speach: String) extends Animal {
+  def sound: String = speach
+}
+
+case class Bear(speach: String) extends Animal {
+  def sound: String = speach
+}
+
+case object ZooKeeper {
+  def openCages: Set[Animal] = Set(new Tiger("prrrr"), new Shark("woosh"), new Bear("grrrr"))
+}
+
+case class Meter(value: Double) extends AnyVal {
+  def toFeet: Foot = Foot(value * 0.3048)
+}
+
+case class Foot(value: Double) extends AnyVal {
+  def toMeter: Meter = Meter(value / 0.3048)
+}
+
 class CaseClassesTest extends FunSuite {
-  trait Animal {
-    def sound: String
-  }
-
-  case class Tiger(speach: String) extends Animal {
-    def sound: String = speach
-  }
-
-  case class Shark(speach: String) extends Animal {
-    def sound: String = speach
-  }
-
-  case class Bear(speach: String) extends Animal {
-    def sound: String = speach
-  }
-
-  object ZooKeeper {
-    def openCages: Set[Animal] = Set(new Tiger("prrrr"), new Shark("woosh"), new Bear("grrrr"))
-  }
-
   test("animals") {
     val animals = ZooKeeper.openCages
     for(animal <- animals) {
@@ -45,5 +53,10 @@ class CaseClassesTest extends FunSuite {
     assert(t1.hashCode != t3.hashCode)
     assert(Tiger.unapply(t1).get == "prrrr")
     assert(t1 == Tiger.apply(t1.sound))
+  }
+
+  test("value classes") {
+    assert(Meter(3.0).toFeet == Foot(0.9144000000000001))
+    assert(Foot(3.0).toMeter == Meter(9.84251968503937))
   }
 }
--- src/test/scala/classes/ClassesTest.scala
+++ src/test/scala/classes/ClassesTest.scala
@@ -2,44 +2,31 @@ package classes
 
 import org.scalatest.FunSuite
 
-case class Meter(value: Double) extends AnyVal {
-  def toFeet: Foot = Foot(value * 0.3048)
+abstract class Car {
+  def sound: String
 }
 
-case class Foot(value: Double) extends AnyVal {
-  def toMeter: Meter = Meter(value / 0.3048)
+class Porsche extends Car {
+  def sound: String = "prrrr"
+}
+
+class Corvette extends Car {
+  def sound: String = "woosh"
+}
+
+class Maserati extends Car {
+  def sound: String = "grrrr"
+}
+
+object Owner {
+  def startEngines: Set[Car] = Set(new Porsche, new Corvette, new Maserati)
 }
 
 class ClassesTest extends FunSuite {
-  abstract class Animal {
-    def sound: String
-  }
-
-  class Tiger extends Animal {
-    def sound: String = "prrrr"
-  }
-
-  class Shark extends Animal {
-    def sound: String = "woosh"
-  }
-
-  class Bear extends Animal {
-    def sound: String = "grrrr"
-  }
-
-  object ZooKeeper {
-    def openCages: Set[Animal] = Set(new Tiger, new Shark, new Bear)
-  }
-
   test("classes") {
-    val animals = ZooKeeper.openCages
-    for (animal <- animals) {
+    val cars = Owner.startEngines
+    for (animal <- cars) {
       assert(animal.sound.length > 0)
     }
-  }
-
-  test("value classes") {
-    assert(Meter(3.0).toFeet == Foot(0.9144000000000001))
-    assert(Foot(3.0).toMeter == Meter(9.84251968503937))
   }
 }
