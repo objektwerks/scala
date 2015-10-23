917510d1e9208ce7301e5a76c7c4795eed8433bf
objektwerks <objektwerks@gmail.com>
2015-10-23 15:33:04 -0400

    completed product and sum type patterns

--- src/test/scala/types/TypesTest.scala
+++ src/test/scala/types/TypesTest.scala
@@ -5,16 +5,32 @@ import org.scalatest.FunSuite
 // Product | Has-A-And Pattern
 trait C
 trait B
-trait A {
-  def b: B
+trait A { // A has-a B and C
+def b: B
   def c: C
 }
-case class D(b: B, c: C)
+case class D(b: B, c: C)  // D has-a B and C
+
+// Product | Has-A-Or Pattern
+sealed trait P
+trait O {
+  def p: P  // O has-a Q or R
+}
+final case class Q(q: Any) extends P
+final case class R(r: Any) extends P
+
+final case class H(b: B) extends P  // O is-a H or I, and H has-a B and I has-a C
+final case class I(c: C) extends P
 
 // Sum | Is-A-Or Pattern
-sealed trait Z
+sealed trait Z  // Z is-a X or Y
 final case class X(x: Any) extends Z
 final case class Y(y: Any) extends Z
+
+// Sum | Is-A-And Pattern
+trait L
+trait M
+trait N extends L with M  // N is-a L and M
 
 // Variance
 trait Relative
