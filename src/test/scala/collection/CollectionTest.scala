e2d1a4c4baa89659f6c2e29435ef46e45aaf425f
objektwerks <objektwerks@gmail.com>
2015-10-30 11:14:11 -0400

    enhanced range test

--- src/test/scala/collection/CollectionTest.scala
+++ src/test/scala/collection/CollectionTest.scala
@@ -386,6 +386,7 @@ class CollectionTest extends FunSuite {
   test("range") {
     assert((1 until 10) == Range(1, 10, 1))
     assert((10 until 1 by -1) == Range(10, 1, -1))
+    assert((1 to 10) == Range.inclusive(1, 10, 1))
   }
 
   test("par set") {
