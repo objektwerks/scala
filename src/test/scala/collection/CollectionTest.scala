6d8d67c113261d0ff3b1e1b52afb9b2b4ba78c50
objektwerks <objektwerks@gmail.com>
2015-10-30 08:14:54 -0400

    refactored collection test code

--- src/test/scala/collection/CollectionTest.scala
+++ src/test/scala/collection/CollectionTest.scala
@@ -297,36 +297,17 @@ class CollectionTest extends FunSuite {
     map.foreach(t => assert(t._1.length == 1 && t._2 > 0))
   }
 
+  test("forall") {
+    val map = Map(1 -> 1, 2 -> 2, 3 -> 3)
+    assert(map.forall( _._2 > 0 ))
+  }
+
   test("for") {
     for (i <- 1 to 3) assert(i == i)
     val set = Set(1, 2, 3)
     for (v <- set) assert(v == v)
     val map = Map(1 -> 1, 2 -> 2, 3 -> 3)
     for (k <- map.keys; v <- map.values) assert(k == k && v == v)
-  }
-
-  test("forall") {
-    val map = Map(1 -> 1, 2 -> 2, 3 -> 3)
-    assert(map.forall( _._2 > 0 ))
-  }
-
-  test("for > flatmap > map") {
-    val xs = List(2, 4)
-    val ys = List(3, 5)
-    val forList = for (x <- xs; y <- ys) yield x * y
-    val mapList = xs flatMap { e => ys map { o => e * o } }
-    assert(forList == List(2 * 3, 2 * 5, 4 * 3, 4 * 5))
-    assert(mapList == List(2 * 3, 2 * 5, 4 * 3, 4 * 5))
-  }
-
-  test("for > flatmap > flatmap > map") {
-    val xs = List(2, 4)
-    val ys = List(3, 5)
-    val zs = List(1, 6)
-    val forList = for (x <- xs; y <- ys; z <- zs) yield x * y * z
-    val mapList = xs flatMap { x => ys flatMap { y => { zs map { z => x * y * z } } } }
-    assert(forList == List(6, 36, 10, 60, 12, 72, 20, 120))
-    assert(mapList == List(6, 36, 10, 60, 12, 72, 20, 120))
   }
 
   test("for > foreach > map") {
@@ -339,14 +320,6 @@ class CollectionTest extends FunSuite {
     xs map (_ * 2) foreach (x => mapList += x)
     assert(forList == mutable.ListBuffer(2, 4))
     assert(mapList == mutable.ListBuffer(2, 4))
-  }
-
-  test("for > if guard > filter") {
-    val letters = List("A", "B", "C", "D", "F")
-    val forLetters: List[Option[String]] = for (l <- letters if l == "A") yield Some(l)
-    val filterLetters = letters filter (l => l == "A") map (l => Some(l))
-    assert(forLetters.head.getOrElse("Z") == "A")
-    assert(filterLetters.head.getOrElse("Z") == "A")
   }
 
   test("for comprehension") {
@@ -362,6 +335,33 @@ class CollectionTest extends FunSuite {
       a <- sas
     } yield a * 2
     assert(bs == as.flatMap(_.map( _ * 2)))
+  }
+
+  test("for comprehension > flatmap > map") {
+    val xs = List(2, 4)
+    val ys = List(3, 5)
+    val forList = for (x <- xs; y <- ys) yield x * y
+    val mapList = xs flatMap { e => ys map { o => e * o } }
+    assert(forList == List(2 * 3, 2 * 5, 4 * 3, 4 * 5))
+    assert(mapList == List(2 * 3, 2 * 5, 4 * 3, 4 * 5))
+  }
+
+  test("for comprehension > flatmap > flatmap > map") {
+    val xs = List(2, 4)
+    val ys = List(3, 5)
+    val zs = List(1, 6)
+    val forList = for (x <- xs; y <- ys; z <- zs) yield x * y * z
+    val mapList = xs flatMap { x => ys flatMap { y => { zs map { z => x * y * z } } } }
+    assert(forList == List(6, 36, 10, 60, 12, 72, 20, 120))
+    assert(mapList == List(6, 36, 10, 60, 12, 72, 20, 120))
+  }
+
+  test("for comprehension > if guard > filter") {
+    val letters = List("A", "B", "C", "D", "F")
+    val forLetters: List[Option[String]] = for (l <- letters if l == "A") yield Some(l)
+    val filterLetters = letters filter (l => l == "A") map (l => Some(l))
+    assert(forLetters.head.getOrElse("Z") == "A")
+    assert(filterLetters.head.getOrElse("Z") == "A")
   }
 
   test("par set") {
