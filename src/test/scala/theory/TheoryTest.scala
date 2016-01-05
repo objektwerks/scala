9f637b6b8c9d95a28f0a42e3aa307670ec49f373
objektwerks <@Mikey1213!>
2016-01-05 06:48:17 -0500

    reordered code

--- src/test/scala/theory/TheoryTest.scala
+++ src/test/scala/theory/TheoryTest.scala
@@ -71,33 +71,6 @@ object Theory {
 }
 
 class TheoryTest extends FunSuite {
-  test("applicative") {
-    val option: Option[Int] = optionApplicative.point(1)
-    val mappedOption: Option[Int] = optionApplicative.map(option)(i => i * 3)
-    assert(option.get == 1)
-    assert(mappedOption.get == 3)
-  }
-
-  test("functor") {
-    val listOfNumbers = List(1, 2, 3)
-    val listOfStrings = listFunctor.map(listOfNumbers)(_.toString)
-    val expectedMorphism = List("1", "2", "3")
-    assert(listOfStrings == expectedMorphism)
-  }
-
-  test("monad") {
-    val option: Option[Int] = optionMonad.point(1)
-    val mappedOption: Option[Int] = optionMonad.map(option)(i => i * 3)
-    val flattenedOption: Option[Int] = optionMonad.flatten(Option(option))
-    val flatMappedOption: Option[Int] = optionMonad.flatMap(option)(i => Some(i))
-    assert(option.get == 1)
-    assert(flattenedOption.get == 1)
-    assert(mappedOption.get == 3)
-    assert(flatMappedOption.get == 1)
-    assert(option != mappedOption)
-    assert(option == flatMappedOption)
-  }
-
   test("monoid") {
     assert(adderMonoid.append(1, 1) == 2)
     assert(adderMonoid.zero == 0)
@@ -120,5 +93,32 @@ class TheoryTest extends FunSuite {
   test("is associative") {
     assert(isAssociative[Int](_ + _, 1, 2, 3))
     assert(!isAssociative[Double](_ / _, 1, 2, 3))
+  }
+
+  test("functor") {
+    val listOfNumbers = List(1, 2, 3)
+    val listOfStrings = listFunctor.map(listOfNumbers)(_.toString)
+    val expectedMorphism = List("1", "2", "3")
+    assert(listOfStrings == expectedMorphism)
+  }
+
+  test("applicative") {
+    val option: Option[Int] = optionApplicative.point(1)
+    val mappedOption: Option[Int] = optionApplicative.map(option)(i => i * 3)
+    assert(option.get == 1)
+    assert(mappedOption.get == 3)
+  }
+
+  test("monad") {
+    val option: Option[Int] = optionMonad.point(1)
+    val mappedOption: Option[Int] = optionMonad.map(option)(i => i * 3)
+    val flattenedOption: Option[Int] = optionMonad.flatten(Option(option))
+    val flatMappedOption: Option[Int] = optionMonad.flatMap(option)(i => Some(i))
+    assert(option.get == 1)
+    assert(flattenedOption.get == 1)
+    assert(mappedOption.get == 3)
+    assert(flatMappedOption.get == 1)
+    assert(option != mappedOption)
+    assert(option == flatMappedOption)
   }
 }
