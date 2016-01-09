58be5909952ac8f1f8d111e0ccb93f20023eefcf
objektwerks <objektwerks@gmail.com>
2016-01-09 13:06:50 -0500

    added execution context to async test

--- src/test/scala/future/AsyncTest.scala
+++ src/test/scala/future/AsyncTest.scala
@@ -3,9 +3,12 @@ package future
 import org.scalatest.FunSuite
 
 import scala.async.Async._
+import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success}
 
 class AsyncTest extends FunSuite {
+  private implicit val ec = ExecutionContext.global
+
   test("async") {
     val future = async {
       val futureOne = async { 1 }
