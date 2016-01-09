8bf0033bebeb09e12cb06e8778181a0e6f0c7aff
objektwerks <objektwerks@gmail.com>
2016-01-09 16:07:45 -0500

    refactored async test; added parallel test

--- src/test/scala/future/AsyncTest.scala
+++ src/test/scala/future/AsyncTest.scala
@@ -3,19 +3,31 @@ package future
 import org.scalatest.FunSuite
 
 import scala.async.Async._
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Future, ExecutionContext}
 import scala.util.{Failure, Success}
 
 class AsyncTest extends FunSuite {
   private implicit val ec = ExecutionContext.global
 
-  test("async") {
+  test("sequential") {
     val future = async {
       val futureOne = async { 1 }
       val futureTwo = async { 2 }
       await(futureOne) + await(futureTwo)
     }
     future onComplete {
+      case Success(result) => assert(result == 3)
+      case Failure(failure) => throw failure
+    }
+  }
+
+  test("parallel") {
+    val futureOne = async { 1 }
+    val futureTwo = async { 2 }
+    val futureThree = async {
+      await(futureOne) + await(futureTwo)
+    }
+    futureThree onComplete {
       case Success(result) => assert(result == 3)
       case Failure(failure) => throw failure
     }
