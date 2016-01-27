2b8324340df7e4d4a1c18345c1c4da9a92f9c476
objektwerks <objektwerks@gmail.com>
2016-01-27 15:50:58 -0500

    refactored patterns

--- src/test/scala/types/PatternsTest.scala
+++ src/test/scala/types/PatternsTest.scala
@@ -9,22 +9,19 @@ trait A { // A has-a B and C
   def b: B
   def c: C
 }
-case class D(b: B, c: C)  // D has-a B and C
 
 // Product | Has-A-Or Pattern
 sealed trait P
-final case class Q(q: Any) extends P
-final case class R(r: Any) extends P
+case class Q(q: Any) extends P
+case class R(r: Any) extends P
 trait O {  // O has-a Q or R
   def p: P
 }
-final case class H(b: B) extends P  // O is-a H or I, and H has-a B and I has-a C
-final case class I(c: C) extends P
 
 // Sum | Is-A-Or Pattern
 sealed trait Z  // Z is-a X or Y
-final case class X(x: Any) extends Z
-final case class Y(y: Any) extends Z
+case class X(x: Any) extends Z
+case class Y(y: Any) extends Z
 
 // Sum | Is-A-And Pattern
 trait L
