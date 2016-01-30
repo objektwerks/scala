9d2a4d316803f1b63b46d1bfe659a83a305b9c52
objektwerks <objektwerks@gmail.com>
2016-01-30 09:04:25 -0500

    added from file io test

--- src/test/scala/io/IOTest.scala
+++ src/test/scala/io/IOTest.scala
@@ -5,6 +5,12 @@ import org.scalatest.FunSuite
 import scala.io.Source
 
 class IOTest extends FunSuite {
+  test("from file") {
+    val license = Source.fromFile("./LICENSE").mkString
+    val words = license.split("\\P{L}+")
+    assert(words.size == 168)
+  }
+
   test("from input stream") {
     val license = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString
     val words = license.split("\\P{L}+")
