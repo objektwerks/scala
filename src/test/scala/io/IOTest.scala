2e5733ad032860d1102086d7da8e6e423c67d0c9
objektwerks <objektwerks@gmail.com>
2016-01-30 09:12:17 -0500

    refactored io tests

--- src/test/scala/io/IOTest.scala
+++ src/test/scala/io/IOTest.scala
@@ -6,14 +6,12 @@ import scala.io.Source
 
 class IOTest extends FunSuite {
   test("from file") {
-    val license = Source.fromFile("./LICENSE").mkString
-    val words = license.split("\\P{L}+")
+    val words = Source.fromFile("./LICENSE").mkString.split("\\P{L}+")
     assert(words.size == 168)
   }
 
   test("from input stream") {
-    val license = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString
-    val words = license.split("\\P{L}+")
+    val words = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split("\\P{L}+")
     assert(words.size == 168)
   }
 }
