ba99fabe942d6cc21ae68acb15e2306d4d906b7c
objektwerks <objektwerks@gmai.com>
2016-02-16 08:39:26 -0500

    refactored transaction

--- src/test/scala/classes/Bank.scala
+++ src/test/scala/classes/Bank.scala
@@ -1,15 +1,15 @@
 package classes
 
-import java.time.LocalDate
+import java.time.{LocalDateTime, LocalDate}
 
 import scala.util.Try
 
 object Bank {
   def list(): Set[Account] = Set[Account]()
   def enquiry(number: String): Option[Account] = None
-  def deposit(amount: Double, credit: Account): Try[Transaction] = Try(Deposit(LocalDate.now(), amount, credit))
-  def withdrawl(amount: Double, debit: Account): Try[Transaction] = Try(Withdrawl(LocalDate.now(), amount, debit))
-  def transfer(amount: Double, debit: Account, credit: Account): Try[Transaction] = Try(Transfer(LocalDate.now(), amount, debit, credit))
+  def deposit(amount: Double, credit: Account): Try[Transaction] = Try(Deposit(LocalDateTime.now(), amount, credit))
+  def withdrawl(amount: Double, debit: Account): Try[Transaction] = Try(Withdrawl(LocalDateTime.now(), amount, debit))
+  def transfer(amount: Double, debit: Account, credit: Account): Try[Transaction] = Try(Transfer(LocalDateTime.now(), amount, debit, credit))
   implicit def ordering: Ordering[Bank] = Ordering.by(_.number)
 }
 case class Bank(number: String, accounts: Set[Account])
@@ -26,9 +26,9 @@ case class SavingsAccount(number: String, opened: LocalDate, closed: LocalDate, 
 case class MarketAccount(number: String, opened: LocalDate, closed: LocalDate, balance: Double) extends Account
 
 trait Transaction {
-  def on: LocalDate
+  def on: LocalDateTime
   def amount: Double
 }
-case class Deposit(on: LocalDate, amount: Double, credit: Account) extends Transaction
-case class Withdrawl(on: LocalDate, amount: Double, debit: Account) extends Transaction
-case class Transfer(on: LocalDate, amount: Double, debit: Account, credit: Account) extends Transaction
+case class Deposit(on: LocalDateTime, amount: Double, credit: Account) extends Transaction
+case class Withdrawl(on: LocalDateTime, amount: Double, debit: Account) extends Transaction
+case class Transfer(on: LocalDateTime, amount: Double, debit: Account, credit: Account) extends Transaction
