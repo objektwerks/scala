26a4bdd1b85bb0f4bafa231dc98d855317413b95
objektwerks <objektwerks@gmai.com>
2016-02-17 05:54:56 -0500

    added balance and amount case classes with implicit +

--- src/test/scala/classes/Bank.scala
+++ src/test/scala/classes/Bank.scala
@@ -7,9 +7,9 @@ import scala.util.Try
 object Bank {
   def list(): Set[Account] = Set[Account]()
   def enquiry(number: String): Option[Account] = None
-  def deposit(amount: Double, credit: Account): Try[Transaction] = Try(Deposit(LocalDateTime.now(), amount, credit))
-  def withdrawl(amount: Double, debit: Account): Try[Transaction] = Try(Withdrawl(LocalDateTime.now(), amount, debit))
-  def transfer(amount: Double, debit: Account, credit: Account): Try[Transaction] = Try(Transfer(LocalDateTime.now(), amount, debit, credit))
+  def deposit(amount: Amount, credit: Account): Try[Transaction] = Try(Deposit(LocalDateTime.now(), amount, credit))
+  def withdrawl(amount: Amount, debit: Account): Try[Transaction] = Try(Withdrawl(LocalDateTime.now(), amount, debit))
+  def transfer(amount: Amount, debit: Account, credit: Account): Try[Transaction] = Try(Transfer(LocalDateTime.now(), amount, debit, credit))
   implicit def ordering: Ordering[Bank] = Ordering.by(_.number)
 }
 case class Bank(number: String, accounts: Set[Account])
@@ -18,18 +18,26 @@ trait Account {
   def number: String
   def opened: LocalDate
   def closed: LocalDate
-  def balance: Double
+  def balance: Balance
   implicit def ordering: Ordering[Account] = Ordering.by(_.number)
 }
-case class CheckingAccount(number: String, opened: LocalDate, closed: LocalDate, balance: Double) extends Account
-case class SavingsAccount(number: String, opened: LocalDate, closed: LocalDate, balance: Double) extends Account
-case class MarketAccount(number: String, opened: LocalDate, closed: LocalDate, balance: Double) extends Account
+case class CheckingAccount(number: String, opened: LocalDate, closed: LocalDate, balance: Balance) extends Account
+case class SavingsAccount(number: String, opened: LocalDate, closed: LocalDate, balance: Balance) extends Account
+case class MarketAccount(number: String, opened: LocalDate, closed: LocalDate, balance: Balance) extends Account
+
+case class Balance(balance: Double) {
+  implicit def +(other: Balance): Balance = Balance(balance + other.balance)
+}
 
 trait Transaction {
   def on: LocalDateTime
-  def amount: Double
+  def amount: Amount
   implicit def ordering: Ordering[Transaction] = Ordering.by(_.on)
 }
-case class Deposit(on: LocalDateTime, amount: Double, credit: Account) extends Transaction
-case class Withdrawl(on: LocalDateTime, amount: Double, debit: Account) extends Transaction
-case class Transfer(on: LocalDateTime, amount: Double, debit: Account, credit: Account) extends Transaction
+case class Deposit(on: LocalDateTime, amount: Amount, credit: Account) extends Transaction
+case class Withdrawl(on: LocalDateTime, amount: Amount, debit: Account) extends Transaction
+case class Transfer(on: LocalDateTime, amount: Amount, debit: Account, credit: Account) extends Transaction
+
+case class Amount(amount: Double) {
+  implicit def +(other: Amount): Amount = Amount(amount + other.amount)
+}
