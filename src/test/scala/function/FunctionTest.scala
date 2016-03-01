873aab24a9d65b323cf32c6e3935a74646c33172
objektwerks <objektwerks@gmail.com>
2016-03-01 08:29:08 -0500

    extracted functions from functions test

--- src/test/scala/function/FunctionTest.scala
+++ src/test/scala/function/FunctionTest.scala
@@ -1,40 +1,10 @@
 package function
 
 import function.Functions._
-import function.Triangle.Triangle
 import org.scalatest.FunSuite
 
 import scala.annotation.tailrec
 import scala.util.Random
-
-object Triangle extends Enumeration {
-  type Triangle = Value
-  val equilateral, isosceles, scalene = Value
-}
-
-object Functions {
-  type Sides = (Int, Int, Int)
-
-  def identifyTriangle(sides: Sides): Triangle = sides match {
-    case (a, b, c) if a == b && c == b => Triangle.equilateral
-    case (a, b, c) if a == b || c == b => Triangle.isosceles
-    case (a, b, c) if a != b && c != b => Triangle.scalene
-  }
-
-  def selectByIndex(source: List[Int], index: Int): Option[Int] = {
-    @tailrec
-    def loop(source: List[Int], index: Int, acc: Int = 1): Option[Int] = source match {
-      case Nil => None
-      case head :: tail => if (acc == index) Some(head) else loop(tail, index, acc + 1)
-    }
-    loop(source, index)
-  }
-
-  def isIntersectable(source: List[Int], target: List[Int]): Boolean = {
-    val result = for (s <- source if target.contains(s)) yield s
-    source == result
-  }
-}
 
 class FunctionTest extends FunSuite {
   test("literal") {
