dd1a46f134da21feee2f2840adc0c2813882f716
objektwerks <objektwerks@gmail.com>
2016-03-04 07:36:11 -0500

    added homeschool design

---
+++ src/test/scala/design/Homeschool.scala
@@ -0,0 +1,11 @@
+package design
+
+import java.time.{LocalDateTime, LocalDate}
+
+case class Student(name: String, grades: Set[Grade])
+
+case class Grade(grade: Int, school: String, from: LocalDate, to: LocalDate)
+
+case class Course(name: String, tasks: Set[Task])
+
+case class Task(task: String, assigned: LocalDateTime, completed: LocalDateTime)
