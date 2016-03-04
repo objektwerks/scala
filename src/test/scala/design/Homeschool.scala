6498916de4efc34c732460404432398e6e6f7c6e
objektwerks <objektwerks@gmail.com>
2016-03-04 08:17:17 -0500

    enhanced homeschool design

--- src/test/scala/design/Homeschool.scala
+++ src/test/scala/design/Homeschool.scala
@@ -4,7 +4,7 @@ import java.time.{LocalDateTime, LocalDate}
 
 case class Student(name: String, grades: Set[Grade])
 
-case class Grade(grade: Int, school: String, from: LocalDate, to: Option[LocalDate], courses: Set[Course])
+case class Grade(year: Int, school: String, from: LocalDate, to: Option[LocalDate], courses: Set[Course])
 
 case class Course(name: String, tasks: Set[Task])
 
