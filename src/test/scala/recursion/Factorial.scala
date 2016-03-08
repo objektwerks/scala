adf896f775d8d112d9def06c9b211472a11baca0
objektwerks <objektwerks@gmail.com>
2016-03-08 07:16:28 -0500

    simplified factorial impls

--- src/test/scala/recursion/Factorial.scala
+++ src/test/scala/recursion/Factorial.scala
@@ -4,13 +4,13 @@ import scala.annotation.tailrec
 
 object Factorial {
   def nonTailCallable(n: Long): Long = n match {
-    case i if i <= 0 => 1
+    case i if i < 1 => 1
     case _ => n * nonTailCallable(n - 1)
   }
 
   @tailrec
   def tailCallable(n: Long, acc: Long = 1): Long = n match {
-    case i if i <= 0 => acc
+    case i if i < 1 => acc
     case _ => tailCallable(n - 1, acc * n)
   }
 }
