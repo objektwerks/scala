a085ba911959a58eb0561f360320614bc2920e53
objektwerks <objektwerks@gmail.com>
2016-11-16 13:10:48 -0500

    refactored either test

--- src/test/scala/exception/EitherTest.scala
+++ src/test/scala/exception/EitherTest.scala
@@ -2,14 +2,12 @@ package exception
 
 import org.scalatest.FunSuite
 
+import scala.util.Try
+
 class EitherTest extends FunSuite {
   test("either") {
-    def divide(x: Int, y: Int): Either[String, Int] = try {
-      Right(x / y)
-    } catch {
-      case t: Throwable => Left("divide by zero error")
-    }
-    assert(divide(9, 3) == Right(3))
-    assert(divide(9, 0) == Left("divide by zero error"))
+    def divide(x: Int, y: Int): Either[Throwable, Int] = Try(x / y).toEither
+    assert(divide(9, 3).isRight)
+    assert(divide(9, 0).isLeft)
   }
 }
