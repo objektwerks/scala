355bd7dbadc9a9d2302def43e9a9740c87cd2db7
objektwerks <objektwerks@runbox.com>
2016-11-18 11:33:24 -0500

    enhanced either test

--- src/test/scala/exception/EitherTest.scala
+++ src/test/scala/exception/EitherTest.scala
@@ -9,8 +9,9 @@ class EitherTest extends FunSuite {
     def divide(x: Int, y: Int): Either[Throwable, Int] = Try(x / y).toEither
     assert(divide(9, 3).isRight)
     assert(divide(9, 0).isLeft)
+    assert(divide(9, 3).contains(3))
+    assert(divide(9, 3).getOrElse(-1) == 3)
     assert(divide(9, 3).map(_ * 3).getOrElse(-1) == 9)
     assert(divide(9, 3).map(_ * 3).right.get == 9)
-    assert(divide(9, 3).contains(3))
   }
 }
