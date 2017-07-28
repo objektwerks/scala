4d6a19b06bef54f009d1f5387aae208a72ed2786
objektwerks <objektwerks@runbox.com>
2017-07-28 11:28:36 -0400

    moved fibonacci into recursion test

--- src/test/scala/recursion/RecursionTest.scala
+++ src/test/scala/recursion/RecursionTest.scala
@@ -3,6 +3,7 @@ package recursion
 import org.scalatest.FunSuite
 
 import scala.annotation.tailrec
+import scala.util.control.TailCalls.{TailRec, done, tailcall}
 
 class RecursionTest extends FunSuite {
   test("structural recursion") {
@@ -50,35 +51,65 @@ class RecursionTest extends FunSuite {
   }
 
   test("naive recursive fibonacci") {
+    def fibonacci(n: Long): BigInt = n match {
+      case 0 | 1 => n
+      case _ => fibonacci(n - 1) + fibonacci(n - 2)
+    }
     val n = 34
-    val f = Fibonacci.naiveRecursive(n)
+    val f = fibonacci(n)
     assert(f.equals(BigInt(5702887)))
     println(s"Naive recursive fibonacci performance slows dramtically using > $n : $f")
   }
 
-  test("tail recursive fibonacci") {
+  test("@tailrec fibonacci") {
+    @tailrec
+    def fibonacci(n: Long, a: Long, b: Long): BigInt = n match {
+      case 0 => a
+      case _ => fibonacci(n - 1, b, a + b)
+    }
     val n = 39
-    val f = Fibonacci.tailRecursive(n, 0, 1)
+    val f = fibonacci(n, 0, 1)
     assert(f.equals(BigInt(63245986)))
     println(s"Tail recursive fibonacci performance is constant using <= $n : $f")
   }
 
-  test("@tailrec fibonacci") {
+  test("@tailrec fibonacci with inner loop") {
+    def fibonacci(n: Long): BigInt = {
+      @tailrec
+      def loop(n: Long, a: Long, b: Long): BigInt = n match {
+        case 0 => a
+        case _ => loop(n - 1, b, a + b)
+      }
+      loop(n, 0, 1)
+    }
     val n = 39
-    val f = Fibonacci.tailRecursiveWithLoop(n)
+    val f = fibonacci(n)
     assert(f.equals(BigInt(63245986)))
     println(s"@tailrec fibonacci performance is constant using <= $n : $f")
   }
 
   test("tailcalls fibonacci") {
+    def fibonacci(n: Long): TailRec[Long] = {
+      if (n < 2) done(n)
+      else for {
+        x <- tailcall(fibonacci(n - 1))
+        y <- tailcall(fibonacci(n - 2))
+      } yield x + y
+    }
     val n = 13
-    val f = Fibonacci.tailcalls(n).result
+    val f = fibonacci(n).result
     assert(f == 233)
     println(s"@tailcalls ( trampolining ) fibonacci performance is horrible: $n : $f")
   }
 
-  test("fibonacci sequece generator") {
-    val sequence = Fibonacci.sequence()
+  test("fibonacci sequence generator") {
+    def fibonacci(a: Int = 0, b: Int = 1): List[Int] = {
+      def build(a: Int, b: Int): Stream[Int] = {
+        Stream.cons(a, build(b, a + b))
+      }
+      build(a, b).takeWhile(_ >= 0).toList
+    }
+    val sequence = fibonacci()
     assert(sequence.size == 47)
     println("Fibonacci sequence: " + sequence)
   }
