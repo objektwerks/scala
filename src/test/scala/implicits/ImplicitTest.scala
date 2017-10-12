be76641684291d4d85a81fe28e64bdcc1036c3ec
objektwerks <objektwerks@runbox.com>
2017-10-12 10:30:49 -0400

    refactored test

--- src/test/scala/implicits/ImplicitTest.scala
+++ src/test/scala/implicits/ImplicitTest.scala
@@ -57,7 +57,7 @@ class ImplicitTest extends FunSuite {
 
   test("implicit ordering") {
     val unsorted = List(Worker("c", "z"), Worker("b", "y"), Worker("a", "x"))
-    assert(unsorted.sorted.head == Worker("a", "x"))
+    assert(unsorted.sorted.min == Worker("a", "x"))
   }
 
   test("implicit folding") {
