63192d59e248c37fbf308cc4399dd02780e577c0
objektwerks <objektwerks@runbox.com>
2017-10-18 11:46:00 -0400

    edited gitignore and built.sbt; refactored sys test

--- src/test/scala/sys/SysTest.scala
+++ src/test/scala/sys/SysTest.scala
@@ -9,19 +9,22 @@ class SysTest extends FunSuite {
   test("system properties") {
     val properties = new SystemProperties
     assert(properties.contains("java.runtime.name"))
+
     properties += ("objekt" -> "werks")
     assert(properties.contains("objekt"))
+
     properties -= "objekt"
     assert(properties.getOrElse("objekt", "empty") == "empty")
   }
 
   test("process") {
-    val file = Process("ls").lineStream.find(f => f == "build.sbt")
+    val file = Process("ls").lineStream.find(file => file == "build.sbt")
     assert(file.getOrElse("empty") == "build.sbt")
+
     val line = Process("find build.sbt").lineStream.headOption
     assert(line.getOrElse("empty") == "build.sbt")
-    val lines = Process("cat .gitignore").lineStream.mkString.split("\\W+").groupBy(l => l).mapValues(l => l.length)
-    assert(lines.values.max == 4)
-    lines foreach println
+
+    val lines = Process("cat .gitignore").lineStream
+    assert(lines.length == 5)
   }
 }
