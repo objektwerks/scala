6e74fd43e262340d08ee27e4636944117bc8e1a0
objektwerks <objektwerks@runbox.com>
2019-08-16 16:57:44 -0400

    refactored try test

--- src/test/scala/exception/TryTest.scala
+++ src/test/scala/exception/TryTest.scala
@@ -8,7 +8,16 @@ import scala.util.control.NonFatal
 import scala.util.{Success, Try, Using}
 
 class TryTest extends FunSuite {
+  def divide(x: String, y: String): Try[Int] = {
+    for {
+      x <- Try(x.toInt)
+      y <- Try(y.toInt)
+    } yield x / y
+  }
+
   def fileToLines(file: String): Try[Seq[String]] = Using(Source.fromFile(file)) { source => source.getLines.toSeq }
+
+  def parseInt(s: String): Option[Int] = Try(s.toInt).toOption
 
   test("try catch handler") {
     val handler: PartialFunction[Throwable, Unit] = {
@@ -18,12 +27,6 @@ class TryTest extends FunSuite {
   }
 
   test("try") {
-    def divide(x: String, y: String): Try[Int] = {
-      for {
-        x <- Try(x.toInt)
-        y <- Try(y.toInt)
-      } yield x / y
-    }
     assert(divide("9", "3").isSuccess)
     assert(divide("9", "3").toOption.contains(3))
     assert(divide("9", "3").get == 3)
@@ -33,7 +36,6 @@ class TryTest extends FunSuite {
   }
 
   test("try option") {
-    def parseInt(s: String): Option[Int] = Try(s.toInt).toOption
     assert(parseInt("a").isEmpty)
     assert(parseInt("1").isDefined)
   }
@@ -44,14 +46,14 @@ class TryTest extends FunSuite {
   }
 
   test("try recover") {
-    val n = for {
-      i <- Try(Integer.parseInt("one")).recover { case _ => 0 }
+    val i = for {
+      i <- Try("one".toInt).recover { case _ => 0 }
     } yield i
-    assert(n == Success(0))
+    assert(i == Success(0))
   }
 
   test("all catch") {
-    assert(allCatch.opt( "1".toInt ).nonEmpty)
-    assert(allCatch.opt( "one".toInt ).isEmpty)
+    assert(allCatch.opt("1".toInt).nonEmpty)
+    assert(allCatch.opt("one".toInt).isEmpty)
   }
 }
