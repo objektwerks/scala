3187394fe0183af1ec502a2055457939c6f124aa
objektwerks <objektwerks@runbox.com>
2019-08-16 17:02:35 -0400

    refactored implicits test

--- src/test/scala/implicits/ImplicitTest.scala
+++ src/test/scala/implicits/ImplicitTest.scala
@@ -29,18 +29,18 @@ object Value {
 }
 
 class ImplicitTest extends FunSuite {
-  test("implicit conversion") {
-    implicit def intToString(i: Int): String = i.toString
-    val three: String = 3
-    assert(three == "3")
-  }
-
   test("implicit parameter") {
     implicit val item = "beers"
     def order(number: Int) (implicit item: String): String = {
       s"$number $item"
     }
     assert(order(2) == "2 beers")
+  }
+
+  test("implicit conversion") {
+    implicit def intToString(i: Int): String = i.toString
+    val three: String = 3
+    assert(three == "3")
   }
 
   test("implicit class") {
@@ -71,10 +71,5 @@ class ImplicitTest extends FunSuite {
     case class Name(name: String)
     implicit val implicitName = Name("Fred Flintstone")
     assert(implicitly[Name] == implicitName)
-  }
-
-  test("package object") {
-    val message = "test"
-    assert(packMessage(message) == s"Message packed: $message")
   }
 }
