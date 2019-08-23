7c823ef9135e3c8382d67266b6800fc24677169f
objektwerks <objektwerks@runbox.com>
2019-08-23 14:33:44 -0400

    added array deque test

--- src/test/scala/collection/ArrayTest.scala
+++ src/test/scala/collection/ArrayTest.scala
@@ -7,20 +7,21 @@ import scala.collection.mutable
 class ArrayTest extends FunSuite with Matchers {
   test("array") {
     val array = Array(1, 2)
-    assert(array.length == 2 && array(0) == 1 && array(1) == 2)
-    assert(array.reverse === Array(2, 1))
-    assert(array === 1 +: Array(2))
-    assert(array === Array(1) :+ 2)
-    assert(array === Array(1) ++ Array(2))
-    assert(array === Array(1) ++: Array(2))
-    assert(3 == (array foldRight 0)(_ + _))
+    1 +: Array(2) shouldEqual array
+    Array(1) :+ 2 shouldEqual array
+    Array(1) ++ Array(2) shouldEqual array
+    Array(1) ++: Array(2) shouldEqual array
   }
 
   test("array buffer") {
     val buffer = mutable.ArrayBuffer(1, 2)
-    assert((buffer += 3) == mutable.ArrayBuffer(1, 2, 3))
-    assert((buffer -= 3) == mutable.ArrayBuffer(1, 2))
-    assert((buffer -= 2) == mutable.ArrayBuffer(1))
-    assert((buffer -= 1) == mutable.ArrayBuffer())
+    (buffer += 3) shouldEqual mutable.ArrayBuffer(1, 2, 3)
+    (buffer -= 3) shouldEqual mutable.ArrayBuffer(1, 2)
+  }
+
+  test("array deque") {
+    val deque = mutable.ArrayDeque(1, 2)
+    (deque += 3) shouldEqual mutable.ArrayDeque(1, 2, 3)
+    (deque -= 3) shouldEqual mutable.ArrayDeque(1, 2)
   }
 }
