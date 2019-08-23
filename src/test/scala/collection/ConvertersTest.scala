56d49ab2a532d11bdaf7709b869f7136ab738e20
objektwerks <objektwerks@runbox.com>
2019-08-23 15:27:14 -0400

    refactored converters test

--- src/test/scala/collection/ConvertersTest.scala
+++ src/test/scala/collection/ConvertersTest.scala
@@ -1,13 +1,24 @@
 package collection
 
-import org.scalatest.FunSuite
+import java.util.function.BinaryOperator
+
+import org.scalatest.{FunSuite, Matchers}
 
 import scala.jdk.CollectionConverters._
 
-class ConvertersTest extends FunSuite {
-  test("converters") {
+class ConvertersTest extends FunSuite with Matchers {
+  test("asJava") {
     val list = List(1, 2, 3).asJava
-    assert(list.size == 3)
-    assert(list.asScala.sum == 6)
+    list.size shouldEqual 3
+    list.stream.count shouldEqual 3
+    list.stream.reduce(new BinaryOperator[Int] {
+      override def apply(t: Int, u: Int): Int = t + u
+    }).get shouldEqual 6
+  }
+
+  test("asScala") {
+    val list = List(1, 2, 3).asJava.asScala
+    list.size shouldEqual 3
+    list.sum shouldEqual 6
   }
 }
