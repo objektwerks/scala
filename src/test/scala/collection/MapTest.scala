395a8c43e805588f32aec7d0abf0aa09106c927c
objektwerks <objektwerks@runbox.com>
2019-08-24 15:33:36 -0400

    switched from assert to matchers

--- src/test/scala/collection/MapTest.scala
+++ src/test/scala/collection/MapTest.scala
@@ -8,41 +8,42 @@ import scala.collection.{SortedMap, mutable}
 class MapTest extends FunSuite with Matchers {
   test("map") {
     val map = Map(1 -> 1, 2 -> 2)
-    assert(map(1) == 1)
-    assert(map(2) == 2)
-    assert(map.getOrElse(3, -1) == -1)
-    assert(map.contains(1))
-    assert(map == Map(1 -> 1) + (2 -> 2))
-    assert(map == Map(1 -> 1, 2 -> 2, 3 -> 3) - 3)
-    assert(map == Map(1 -> 1) ++ Map(2 -> 2))
-    assert(map == Map(1 -> 1, 2 -> 2, 3 -> 3, 4 -> 4) -- List(3, 4))
-    assert(map.keySet == Set(1, 2) && map.values.toSet == Set(1, 2))
-    assert(map.empty.isEmpty)
+    map(1) shouldEqual 1
+    map(2) shouldEqual 2
+    map.getOrElse(3, -1) shouldEqual -1
+    map.contains(1) shouldBe true
+    map shouldEqual Map(1 -> 1) + (2 -> 2)
+    map shouldEqual Map(1 -> 1, 2 -> 2, 3 -> 3) - 3
+    map shouldEqual Map(1 -> 1) ++ Map(2 -> 2)
+    map shouldEqual Map(1 -> 1, 2 -> 2, 3 -> 3, 4 -> 4) -- List(3, 4)
+    map.keySet shouldEqual Set(1, 2)
+    map.values.toSet shouldEqual Set(1, 2)
+    map.empty.isEmpty shouldBe true
   }
 
   test("list map") {
     val map = ListMap(3 -> 3, 2 -> 2, 1 -> 1)
     val list = map.keys.toIndexedSeq
-    assert(list(0) == 3)
-    assert(list(1) == 2)
-    assert(list(2) == 1)
+    list(0) shouldEqual 3
+    list(1) shouldEqual 2
+    list(2) shouldEqual 1
   }
 
   test("sorted map") {
     val map = SortedMap(3 -> 3, 2 -> 2, 1 -> 1)
     val list = map.keys.toIndexedSeq
-    assert(list(0) == 1)
-    assert(list(1) == 2)
-    assert(list(2) == 3)
+    list(0) shouldEqual 1
+    list(1) shouldEqual 2
+    list(2) shouldEqual 3
   }
 
   test("mutable map") {
     val map = mutable.Map(1 -> 1, 2 -> 2)
-    assert((map += 3 -> 3) == Map(1 -> 1, 2 -> 2, 3 -> 3))
-    assert((map -= 3) == Map(1 -> 1, 2 -> 2))
-    assert((map -= 2) == Map(1 -> 1))
-    assert((map -= 1) == Map())
-    assert((map ++= List(1 -> 1, 2 -> 2)) == Map(1 -> 1, 2 -> 2))
-    assert((map --= List(1, 2)) == Map())
+    (map += 3 -> 3) shouldEqual Map(1 -> 1, 2 -> 2, 3 -> 3)
+    (map -= 3) shouldEqual Map(1 -> 1, 2 -> 2)
+    (map -= 2) shouldEqual Map(1 -> 1)
+    (map -= 1) shouldEqual Map()
+    (map ++= List(1 -> 1, 2 -> 2)) shouldEqual Map(1 -> 1, 2 -> 2)
+    (map --= List(1, 2)) shouldEqual Map()
   }
 }
