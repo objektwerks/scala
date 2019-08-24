ea2c09a17311d8fc0e0850d793b0df277eeaf3e6
objektwerks <objektwerks@runbox.com>
2019-08-24 14:41:39 -0400

    switched from assert to matchers

--- src/test/scala/collection/TupleTest.scala
+++ src/test/scala/collection/TupleTest.scala
@@ -2,11 +2,22 @@ package collection
 
 import org.scalatest.{FunSuite, Matchers}
 
+case class CityStateZip(city: String, state: String, zip: Int) {
+  def tupled: (String, String, Int) = (city, state, zip)
+}
+
 class TupleTest extends FunSuite with Matchers {
+  test("tupled") {
+    val (city, state, zip) = CityStateZip("placida", "florida", 33946).tupled
+    city shouldEqual "placida"
+    state shouldEqual "florida"
+    zip shouldEqual 33946
+  }
+
   test("tuple") {
-    val cityStateZip = ("placida", "florida", 33946)
-    assert(cityStateZip._1 == "placida" && cityStateZip._2 == "florida"  && cityStateZip._3 == 33946)
     val (first, last, age) = ("fred", "flintstone", 99)
-    assert(first == "fred" && last == "flintstone" && age == 99)
+    first shouldEqual "fred"
+    last shouldEqual "flintstone"
+    age shouldEqual 99
   }
 }
