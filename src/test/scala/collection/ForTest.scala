5f0a2436e474fc94ba8c841bc2cb404d8b3377de
objektwerks <objektwerks@runbox.com>
2019-08-24 15:09:36 -0400

    switched from assert to matchers

--- src/test/scala/collection/ForTest.scala
+++ src/test/scala/collection/ForTest.scala
@@ -1,29 +1,29 @@
 package collection
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
 import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
 
-class ForTest extends FunSuite {
+class ForTest extends FunSuite with Matchers {
   test("foreach") {
     val map = Map("a" -> 1, "b" -> 2, "c" -> 3)
-    map.foreach(t => assert(t._1.length == 1 && t._2 > 0))
+    map.foreach( t => t._2 should be > 0 )
   }
 
   test("forall") {
     val map = Map(1 -> 1, 2 -> 2, 3 -> 3)
-    assert(map.forall( _._2 > 0 ))
+    map.forall( _._2 > 0 ) shouldBe true
   }
 
   test("for") {
-    for (i <- 1 to 3) assert(i == i)
+    for (i <- 1 to 3) i shouldEqual i
+
     val set = Set(1, 2, 3)
-    for (v <- set) assert(v == v)
+    for (v <- set) v shouldEqual v
+
     val map = Map(1 -> 1, 2 -> 2, 3 -> 3)
-    for ((k, v) <- map) assert(k == k && v == v)
-    for (k <- map.keys; v <- map.values) assert(k == k && v == v)
-    for (e <- map) assert(e._1 > 0 && e._2 > 0)
+    for ((_, v) <- map) v shouldEqual v
   }
 
   test("for > foreach > map") {
@@ -34,8 +34,8 @@ class ForTest extends FunSuite {
     }
     val mapList = mutable.ListBuffer[Int]()
     xs map (_ * 2) foreach (x => mapList += x)
-    assert(forList == mutable.ListBuffer(2, 4))
-    assert(mapList == mutable.ListBuffer(2, 4))
+    forList shouldEqual mutable.ListBuffer(2, 4)
+    mapList shouldEqual mutable.ListBuffer(2, 4)
   }
 
   test("for comprehension") {
@@ -43,14 +43,14 @@ class ForTest extends FunSuite {
     val ys = for {
       x <- xs
     } yield x * 2
-    assert(ys == xs.map(_ * 2))
+    ys shouldEqual xs.map(_ * 2)
 
     val as = List(List(1), List(2, 3), List(4, 5, 6))
     val bs = for {
       sas <- as
       a <- sas
     } yield a * 2
-    assert(bs == as.flatMap(_.map( _ * 2)))
+    bs shouldEqual as.flatMap(_.map( _ * 2))
   }
 
   test("nested for comprehensions") {
@@ -63,9 +63,9 @@ class ForTest extends FunSuite {
         y <- ys
       } yield x + y
     }
-    assert(zs == List(List(5, 6, 7), List(6, 7, 8), List(7, 8, 9)))
-    assert(zs.flatten == List(5, 6, 7, 6, 7, 8, 7, 8, 9))
-    assert(zs.flatten.sum == 63)
+    zs shouldEqual List(List(5, 6, 7), List(6, 7, 8), List(7, 8, 9))
+    zs.flatten shouldEqual List(5, 6, 7, 6, 7, 8, 7, 8, 9)
+    zs.flatten.sum shouldEqual 63
   }
 
   test("for comprehension > map") {
@@ -73,7 +73,7 @@ class ForTest extends FunSuite {
     val c = for {
       x <- o map { i => i * i * i }
     } yield x
-    assert(c.get == 27)
+    c.get shouldEqual 27
   }
 
   test("for comprehension vs flatmap > map") {
@@ -81,8 +81,8 @@ class ForTest extends FunSuite {
     val ys = List(3, 5)
     val forList = for (x <- xs; y <- ys) yield x * y
     val mapList = xs flatMap { e => ys map { o => e * o } }
-    assert(forList == List(2 * 3, 2 * 5, 4 * 3, 4 * 5))
-    assert(mapList == List(2 * 3, 2 * 5, 4 * 3, 4 * 5))
+    forList shouldEqual List(2 * 3, 2 * 5, 4 * 3, 4 * 5)
+    mapList shouldEqual List(2 * 3, 2 * 5, 4 * 3, 4 * 5)
   }
 
   test("for comprehension vs flatmap > flatmap > map") {
@@ -91,28 +91,28 @@ class ForTest extends FunSuite {
     val zs = List(1, 6)
     val forList = for (x <- xs; y <- ys; z <- zs) yield x * y * z
     val mapList = xs flatMap { x => ys flatMap { y => { zs map { z => x * y * z } } } }
-    assert(forList == List(6, 36, 10, 60, 12, 72, 20, 120))
-    assert(mapList == List(6, 36, 10, 60, 12, 72, 20, 120))
+    forList shouldEqual List(6, 36, 10, 60, 12, 72, 20, 120)
+    mapList shouldEqual List(6, 36, 10, 60, 12, 72, 20, 120)
   }
 
   test("for comprehension > if guard filter") {
     val filteredLetters = for (l <- List("A", "B", "C", "D", "F") if l == "A") yield l
     val filteredNumbers = for (n <- List(-2, -1, 0, 1, 2) if n > 0) yield n
-    assert(filteredLetters.head == "A")
-    assert(filteredNumbers == List(1, 2))
+    filteredLetters.head shouldEqual "A"
+    filteredNumbers shouldEqual List(1, 2)
   }
 
   test("for comprehension > zip") {
     val xs = for {
       (a, b) <- List(1, 2, 3) zip List(4, 5, 6)
     } yield a + b
-    assert(xs == List(5, 7, 9))
+    xs shouldEqual List(5, 7, 9)
   }
 
   test("for comprehension with recover") {
     implicit val ec = ExecutionContext.global
     val future = Future(Integer.parseInt("one"))
     val result = ( for { i <- future } yield i ).recover { case _: Throwable => -1 }
-    result foreach { x => assert(x == -1) }
+    result foreach { x => x shouldEqual -1 }
   }
 }
