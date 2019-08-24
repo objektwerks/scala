212a0c3cb4e95548a652de21fa92a8cdbab3212f
objektwerks <objektwerks@runbox.com>
2019-08-24 14:07:21 -0400

    switched from assert to matchers

--- src/test/scala/classes/CaseClassesTest.scala
+++ src/test/scala/classes/CaseClassesTest.scala
@@ -1,63 +1,63 @@
 package classes
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
 sealed trait Animal { def speak: String }
 case class Tiger(speach: String) extends Animal { override def speak: String = speach }
-case class Shark(speach: String) extends Animal { override def speak: String = speach }
+case class Panther(speach: String) extends Animal { override def speak: String = speach }
 case class Bear(speach: String) extends Animal { override def speak: String = speach }
-case object ZooKeeper { def openCages: Set[Animal] = Set(Tiger("prrrr"), Shark("woosh"), Bear("grrrr")) }
+case object ZooKeeper { def openCages: Set[Animal] = Set(Tiger("prrrr"), Panther("woosh"), Bear("grrrr")) }
 
 case class Meter(value: Double) extends AnyVal { def toFeet: Foot = Foot(value * 0.3048) }
 case class Foot(value: Double) extends AnyVal { def toMeter: Meter = Meter(value / 0.3048) }
 
-class CaseClassesTest extends FunSuite {
+class CaseClassesTest extends FunSuite with Matchers {
   test("case classes") {
     val animals = ZooKeeper.openCages
     for(animal <- animals) {
-      assert(animal.speak.nonEmpty)
+      animal.speak.nonEmpty shouldBe true
       animal match {
-        case Tiger(s) => assert(s == "prrrr")
-        case Shark(s) => assert(s == "woosh")
-        case Bear(s) => assert(s == "grrrr")
+        case Tiger(speech) => speech shouldEqual "prrrr"
+        case Panther(speech) => speech shouldEqual "woosh"
+        case Bear(speech) => speech shouldEqual "grrrr"
       }
     }
   }
 
   test("equality") {
-    val t1 = Tiger("prrrr")
-    val t2 = Tiger("prrrr")
-    val t3 = Tiger("meow")
-    assert( (t1 eq t1) && (t2 eq t2) && (t3 eq t3) )
-    assert(t1.equals(t2))
-    assert(!t1.equals(t3))
-    assert(t1 == t2)
-    assert(t1 != t3)
-    assert(t1.hashCode == t2.hashCode)
-    assert(t1.hashCode != t3.hashCode)
+    val t1 = Tiger("roar")
+    val t2 = Tiger("roar")
+    val t3 = Tiger("prrrr")
+    t1 shouldEqual t1
+    t2 shouldEqual t2
+    t3 shouldEqual t3
+    t1 shouldEqual t2
+    t1 should not equal t3
+    t1.hashCode shouldEqual t2.hashCode
+    t1.hashCode should not equal t3.hashCode
   }
 
   test("copy") {
-    val s1 = Shark("woosh")
-    val s2 = s1.copy(speach = "arrrgh")
-    assert(s1 == s1.copy())
-    assert(s1 != s2)
+    val p1 = Panther("prrrr")
+    val p2 = p1.copy(speach = "arrrgh")
+    p1 shouldEqual p1.copy()
+    p1 should not equal p2
   }
 
   test("to string") {
-    val t1 = Tiger("prrrr")
-    val t2 = Tiger("prrrr")
-    assert(t1.toString == t2.toString)
+    val t1 = Tiger("roar")
+    val t2 = Tiger("roar")
+    t1.toString shouldEqual t2.toString
   }
 
   test("apply unapply") {
-    val t1 = Tiger("prrrr")
-    assert(t1 == Tiger.apply(t1.speak))
-    assert(Tiger.unapply(t1).get == "prrrr")
+    val t1 = Tiger("roar")
+    t1 shouldEqual Tiger.apply(t1.speak)
+    Tiger.unapply(t1).get shouldEqual "roar"
   }
 
   test("value classes") {
-    assert(Meter(3.0).toFeet == Foot(0.9144000000000001))
-    assert(Foot(3.0).toMeter == Meter(9.84251968503937))
+    Meter(3.0).toFeet shouldEqual Foot(0.9144000000000001)
+    Foot(3.0).toMeter shouldEqual Meter(9.84251968503937)
   }
 }
