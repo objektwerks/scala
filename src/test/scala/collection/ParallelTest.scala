e24e1d370b3e06371ab25f2d26ce9b9c3efd172f
objektwerks <objektwerks@runbox.com>
2019-08-24 14:45:30 -0400

    switched from assert to matchers

--- src/test/scala/collection/ParallelTest.scala
+++ src/test/scala/collection/ParallelTest.scala
@@ -8,22 +8,22 @@ import scala.collection.parallel.{ParMap, ParSeq, ParSet}
 class ParallelTest extends FunSuite with Matchers {
   test("par set") {
     val set = ParSet(1 to 1000000:_*)
-    assert(set.sum == 1784293664)
+    set.sum shouldEqual 1784293664
   }
 
   test("par map") {
     val m = for (i <- 1 to 1000000) yield (i , i)
     val map = ParMap(m:_*)
-    assert(map.values.sum == 1784293664)
+    map.values.sum shouldEqual 1784293664
   }
 
   test("par seq") {
     val seq = ParSeq(1 to 1000000:_*)
-    assert(seq.sum == 1784293664)
+    seq.sum shouldEqual 1784293664
   }
 
   test("par range") {
     val range = ParRange(1, 1000000, 1, inclusive = true)
-    assert(range.sum == 1784293664)
+    range.sum shouldEqual 1784293664
   }
 }
