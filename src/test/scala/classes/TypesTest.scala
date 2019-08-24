b0a04b13d188bfc4340eb0f4116a948042d25bf3
objektwerks <objektwerks@runbox.com>
2019-08-24 14:28:28 -0400

    switched from assert to matchers

--- src/test/scala/classes/TypesTest.scala
+++ src/test/scala/classes/TypesTest.scala
@@ -1,6 +1,6 @@
 package classes
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
 // Variance
 trait Relative
@@ -47,14 +47,14 @@ object Rational {
       (y.numerator.toDouble / y.denominator.toDouble) )
 }
 
-class TypesTest extends FunSuite {
+class TypesTest extends FunSuite with Matchers {
   test("variance") {
     val covariant: Covariant[Parent] = new Covariant[Child](new Child())
     val contravariant: Contravariant[Child, Parent] = new Contravariant[Child, Parent](new Parent())
     val invariant: Invariant[Child] = new Invariant[Child](new Child())
-    assert(covariant.relative.isInstanceOf[Child])
-    assert(contravariant.relative.isInstanceOf[Parent])
-    assert(invariant.relative.isInstanceOf[Child])
+    covariant.relative.isInstanceOf[Child] shouldBe true
+    contravariant.relative.isInstanceOf[Parent] shouldBe true
+    invariant.relative.isInstanceOf[Child] shouldBe true
   }
 
   test("contravariant in, covariant out") {
@@ -63,42 +63,44 @@ class TypesTest extends FunSuite {
     }
     val numbers = List(-3, -2, -1, 0, 1, 2, 3)
     val positives: List[Int] = numbers.filter(n => filter.isPositive(n))
-    assert(positives == List(1, 2, 3))
+    positives shouldEqual List(1, 2, 3)
   }
 
   test("bounds") {
     val upperBounds: Int = UpperBounds(3)
-    assert(upperBounds == 3)
+    upperBounds shouldEqual 3
+
     val lowerBounds: Any = LowerBounds(3)
-    assert(lowerBounds == 3)
+    lowerBounds shouldEqual 3
   }
 
   test("compound types") {
     val robot = new Robot()
-    assert(robot.isRunning)
-    assert(robot.isEmoting)
-    assert(robot.isSpeaking)
+    robot.isRunning shouldBe true
+    robot.isEmoting shouldBe true
+    robot.isSpeaking shouldBe true
   }
 
   test("type alias") {
     type User = String
     type Age = Int
     val users:  Map[User, Age] =  Map("john" -> 21, "jane" -> 19)
-    assert(users("john") == 21)
-    assert(users("jane") == 19)
+    users("john") shouldEqual 21
+    users("jane") shouldEqual 19
   }
 
   test("duck typing") {
     class Greeter { def greet = "Hi!" }
     def greet(greeter: { def greet: String } ): String = greeter.greet
-    assert(greet(new Greeter) == "Hi!")
+    greet(new Greeter) shouldEqual "Hi!"
   }
 
   test("self type") {
     val hello = new Speaker() with Hello
-    assert(hello.greet == "hello")
+    hello.greet shouldEqual "hello"
+
     val goodbye = new Speaker() with Goodbye
-    assert(goodbye.greet == "goodbye")
+    goodbye.greet shouldEqual "goodbye"
   }
 
   test("path dependent types") {
@@ -106,22 +108,22 @@ class TypesTest extends FunSuite {
     val path1 = new first1.Second()
     val first2 = new First()
     val path2 = new first2.Second()
-    assert(path1 != path2)
+    path1 should not equal path2
   }
 
   test("type instances") {
     val minOrdering: Ordering[Int] = Ordering.fromLessThan[Int](_ < _)
     val maxOrdering: Ordering[Int] = Ordering.fromLessThan[Int](_ > _)
-    assert(List(3, 4, 2).sorted(minOrdering) == List( 2, 3, 4))
-    assert(List(3, 4, 2).sorted(maxOrdering) == List(4, 3, 2))
+    List(3, 4, 2).sorted(minOrdering) shouldEqual List( 2, 3, 4)
+    List(3, 4, 2).sorted(maxOrdering) shouldEqual List(4, 3, 2)
   }
 
   test("implicit type instance") {
     implicit val ordering: Ordering[String] = Ordering.fromLessThan[String](_ < _)
-    assert(List("c", "b", "a").sorted == List("a", "b", "c"))
+    List("c", "b", "a").sorted shouldEqual List("a", "b", "c")
   }
 
   test("companion object with implicit") {
-    assert(List(Rational(1, 2), Rational(3, 4), Rational(1, 3)).sorted == List(Rational(1, 3), Rational(1, 2), Rational(3, 4)))
+    List(Rational(1, 2), Rational(3, 4), Rational(1, 3)).sorted shouldEqual List(Rational(1, 3), Rational(1, 2), Rational(3, 4))
   }
 }
