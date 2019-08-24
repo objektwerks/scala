1d7ec8aee65147d48d53a402210537f934608e1c
objektwerks <objektwerks@runbox.com>
2019-08-24 11:35:16 -0400

    switched from assert to matchers

--- src/test/scala/exception/EitherTest.scala
+++ src/test/scala/exception/EitherTest.scala
@@ -1,23 +1,23 @@
 package exception
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
 import scala.util.Try
 
-class EitherTest extends FunSuite {
+class EitherTest extends FunSuite with Matchers {
   def divide(x: Int, y: Int): Either[Throwable, Int] = Try(x / y).toEither
 
   test("either") {
-    assert(divide(9, 3).isRight)
-    assert(divide(9, 0).isLeft)
-    assert(divide(9, 3).contains(3))
-    assert(divide(9, 3).exists(_ == 3))
-    assert(divide(9, 3).getOrElse(-1) == 3)
-    assert(divide(9, 3).map(_ * 3).getOrElse(-1) == 9)
-    assert(divide(9, 3).map(_ * 3).filterOrElse(_ == 9, -1).getOrElse(-1) == 9)
+    divide(9, 3).isRight shouldBe true
+    divide(9, 0).isLeft shouldBe true
+    divide(9, 3).contains(3) shouldBe true
+    divide(9, 3).exists(_ == 3) shouldBe true
+    divide(9, 3).getOrElse(-1) shouldEqual 3
+    divide(9, 3).map(_ * 3).getOrElse(-1) shouldEqual 9
+    divide(9, 3).map(_ * 3).filterOrElse(_ == 9, -1).getOrElse(-1) shouldEqual 9
     divide(3, 0) match {
       case Right(_) => throw new Exception("Should be divide by zero error.")
-      case Left(error) => assert(error.isInstanceOf[Throwable])
+      case Left(error) => error.isInstanceOf[Throwable] shouldBe true
     }
   }
 }
