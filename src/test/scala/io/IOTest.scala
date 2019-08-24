a1a4ab8c4dc6d8f5780d66abd9312b5c269c9b89
objektwerks <objektwerks@runbox.com>
2019-08-24 10:56:45 -0400

    switched from assert to matchers; resolved regex error caused by using scalatest matchers

--- src/test/scala/io/IOTest.scala
+++ src/test/scala/io/IOTest.scala
@@ -1,56 +1,56 @@
 package io
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
 import scala.collection.MapView
 import scala.io.{Codec, Source}
 import scala.util.{Try, Using}
 
-class IOTest extends FunSuite {
-  val regex = "\\P{L}+"
+class IOTest extends FunSuite with Matchers {
   val quote = "You can avoid reality, but you cannot avoid the consequences of avoiding reality."
 
   test("from url") {
-    val jokes = Source.fromURL("http://api.icndb.com/jokes/random/").mkString.split(regex)
-    assert(jokes.nonEmpty)
+    val jokes = Source.fromURL("http://api.icndb.com/jokes/random/").mkString.split("\\W+")
+    jokes.nonEmpty shouldBe true
   }
 
   test("from file") {
-    val words = Source.fromFile("./LICENSE").mkString.split(regex)
-    assert(words.length == 168)
+    val words = Source.fromFile("./LICENSE").mkString.split("\\W+")
+    words.length shouldEqual 169
   }
 
   test("from input stream") {
-    val words = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split(regex)
-    assert(words.length == 168)
-    assert(toWordCountMap(words).size == 95)
+    val words = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split("\\W+")
+    words.length shouldEqual 169
+    toWordCountMap(words).size shouldEqual 96
   }
 
   test("from string") {
-    val words = Source.fromString(quote).mkString.split(regex)
-    assert(words.length == 13)
+    val words = Source.fromString(quote).mkString.split("\\W+")
+    words.length shouldEqual 13
   }
 
   test("from chars") {
-    val words = Source.fromChars(quote.toCharArray).mkString.split(regex)
-    assert(words.length == 13)
+    val words = Source.fromChars(quote.toCharArray).mkString.split("\\W+")
+    words.length shouldEqual 13
   }
 
   test("from bytes") {
-    val words = Source.fromBytes(quote.getBytes(Codec.UTF8.name)).mkString.split(regex)
-    assert(words.length == 13)
+    val words = Source.fromBytes(quote.getBytes(Codec.UTF8.name)).mkString.split("\\W+")
+    words.length shouldEqual 13
   }
 
   test("grouped") {
-    val list = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split(regex).toList
-    assert(list.length == 168)
+    val list = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).mkString.split("\\W+").toList
+    list.length shouldEqual 169
+
     val words = list.grouped(list.length / 8).toList
-    assert(words.length == 8)
+    words.length shouldEqual 9
   }
 
   test("file to lines") {
-    assert(fileToLines("build.sbt").isSuccess)
-    assert(fileToLines("sbt.sbt").isFailure)
+    fileToLines("build.sbt").isSuccess shouldBe true
+    fileToLines("sbt.sbt").isFailure shouldBe true
   }
 
   def toWordCountMap(words: Array[String]): MapView[String, Int] = {
