b7769b0c0c3b680490894a01760d725047468075
objektwerks <objektwerks@runbox.com>
2021-01-05 09:06:01 -0500

    enhanced newBuilder test

--- src/test/scala/collection/CollectionTest.scala
+++ src/test/scala/collection/CollectionTest.scala
@@ -3,6 +3,7 @@ package collection
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import scala.collection.View
 import scala.collection.concurrent
 import scala.collection.immutable._
 import scala.collection.mutable
@@ -18,6 +19,7 @@ class CollectionTest extends AnyFunSuite with Matchers {
     LinearSeq.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe LinearSeq(1, 2, 3)
     Set.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe Set(1, 2, 3)
     Map.newBuilder[Int, Int].addOne(1 -> 1).addOne(2 -> 2).addOne(3 -> 3).result() shouldBe Map(1 -> 1, 2 -> 2, 3 -> 3)
+    View.newBuilder[Int].addOne(1).addOne(2).addOne(3).result().toList shouldBe List(1, 2, 3)
   }
 
   test("list") {
