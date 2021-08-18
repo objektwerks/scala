9247124cf944780e147bdf6a997299a995ac0fe3
objektwerks <objektwerks@runbox.com>
2021-08-18 11:06:53 -0400

    added decode to RLE impl

--- src/test/scala/encoding/RLETest.scala
+++ src/test/scala/encoding/RLETest.scala
@@ -18,15 +18,38 @@ object Encoding {
     val valueAsChars = value.toCharArray.toList
     val encodings = group(valueAsChars) map { chars => Encoding(chars.head, chars.length) }
     val encodedValues = encodings map { group =>
-      group.char.toString + ( if (group.count == 1) "" else group.count.toString )
+      group.char.toString + group.count.toString
     }
     encodedValues.mkString
   }
+
+  def decode(value: String): String = {
+    var decoded: String = ""
+    var count: Int = 0
+    val result = new StringBuilder()
+    value.toCharArray.toList.foreach { char =>
+      if ( char.isDigit ) {
+        count = char.asDigit
+        result.append( decoded * count )
+      } else {
+        decoded = char.toString
+      }
+    }
+    result.mkString
+  }
 }
 
+/**
+ * Encoding for single letter occurences includes a 1 so that decode works correctly.
+ */
 class RLETest extends AnyFunSuite with Matchers {
   test("encode") {
     println( s" *** Run Length Encoding: ${ Encoding.encode("aaaabbcccaeeeee") }" )
-    Encoding.encode("aaaabbcccaeeeee") shouldBe "a4b2c3ae5"
+    Encoding.encode("aaaabbcccaeeeee") shouldBe "a4b2c3a1e5"
+  }
+
+  test("decode") {
+    println( s" *** Run Length Decoding: ${ Encoding.decode("a4b2c3a1e5") }" )
+    Encoding.decode("a4b2c3a1e5") shouldBe "aaaabbcccaeeeee"
   }
 }
