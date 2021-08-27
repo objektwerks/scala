54c5152afa764c83bc43482515d6bb7a21642a3d
objektwerks <objektwerks@runbox.com>
2021-08-27 16:55:17 -0400

    enhanced rle test

--- src/test/scala/encoding/RLETest.scala
+++ src/test/scala/encoding/RLETest.scala
@@ -35,7 +35,7 @@ object RLE {
       if ( char.isDigit ) {
         count = char.asDigit
         result.append( decoded * count )
-      } else {
+      } else if ( char.isLetter ) {
         decoded = char.toString
       }
     }
