7092144551f291f407076ff70afb7cd8bb528759
objektwerks <objektwerks@runbox.com>
2021-08-27 17:44:02 -0400

    enhanced rle test

--- src/test/scala/encoding/RLETest.scala
+++ src/test/scala/encoding/RLETest.scala
@@ -50,11 +50,13 @@ class RLETest extends AnyFunSuite with Matchers {
     println( s"*** Run Length Encoding: ${ RLE.encode("aaaabbcccaeeeee") }" )
     RLE.encode("aaaabbcccaeeeee") shouldBe "a4b2c3a1e5"
     RLE.encode("") shouldBe ""
+    println( s"*** Run Length Encoding: ${ RLE.encode("aaaaaaaaaaaaa") }" )
   }
 
   test("decode") {
     println( s"*** Run Length Decoding: ${ RLE.decode("a4b2c3a1e5") }" )
     RLE.decode("a4b2c3a1e5") shouldBe "aaaabbcccaeeeee"
     RLE.decode("") shouldBe ""
+    println( s"*** Run Length Decoding: ${ RLE.encode("a17") }" )
   }
 }
