2716a1e294a344e6a9c087b1f88e3d8d9c00461e
objektwerks <objektwerks@runbox.com>
2021-11-29 08:37:53 -0500

    implemented dependent type test

--- src/test/scala/types/DependentTypeTest.scala
+++ src/test/scala/types/DependentTypeTest.scala
@@ -3,8 +3,28 @@ package types
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+trait Box {
+  type Value
+  val value: Value
+}
+
+object Box {
+  def deriveValue(box: Box): box.Value = box.value
+}
+
 class DependentTypeTest extends AnyFunSuite with Matchers {
   test("dependent") {
-    // TODO
+    import Box._
+
+    def valueOf[T](t: T) = new Box {
+      type Value = T
+      val value: T = t
+    }
+
+    val intValue    = valueOf(1)
+    val stringValue = valueOf("One")
+
+    assert(deriveValue(intValue) == 1)
+    assert(deriveValue(stringValue) == "One")
   }
 }
