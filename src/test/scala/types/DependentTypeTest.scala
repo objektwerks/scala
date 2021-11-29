409325e2236338b636834546a1efbfed1ba0c8d5
objektwerks <objektwerks@runbox.com>
2021-11-29 08:28:30 -0500

    added dependent type test ... todo

---
+++ src/test/scala/types/DependentTypeTest.scala
@@ -0,0 +1,10 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class DependentTypeTest extends AnyFunSuite with Matchers {
+  test("dependent") {
+    // TODO
+  }
+}
