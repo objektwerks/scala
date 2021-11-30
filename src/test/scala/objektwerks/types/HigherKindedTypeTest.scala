c4bd786d800b3eb957df37e9cb4dd517888cb699
objektwerks <objektwerks@runbox.com>
2021-11-30 07:24:01 -0500

    corrected class naming error

--- src/test/scala/objektwerks/types/HigherKindedTypeTest.scala
+++ src/test/scala/objektwerks/types/HigherKindedTypeTest.scala
@@ -3,7 +3,7 @@ package objektwerks.types
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class HigerKindedTypeTest extends AnyFunSuite with Matchers {
+class HigherKindedTypeTest extends AnyFunSuite with Matchers {
   trait Functor[F[_]] {
     def map[A, B](fa: F[A])(f: A => B): F[B]
   }
