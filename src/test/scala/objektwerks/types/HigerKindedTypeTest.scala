94341654269ac89288d8151633d56e4e577aa635
objektwerks <objektwerks@runbox.com>
2021-11-30 07:20:03 -0500

    implemented higher kinded test

--- src/test/scala/objektwerks/types/HigerKindedTypeTest.scala
+++ src/test/scala/objektwerks/types/HigerKindedTypeTest.scala
@@ -4,7 +4,18 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class HigerKindedTypeTest extends AnyFunSuite with Matchers {
+  trait Functor[F[_]] {
+    def map[A, B](fa: F[A])(f: A => B): F[B]
+  }
+
+  val listFunctor = new Functor[List] {
+    override def map[A, B](xs: List[A])(f: A => B): List[B] = xs map f
+  }
+
   test("higher kinded") {
-    // TODO
+    val listOfNumbers = List(1, 2, 3)
+    val listOfStrings = listFunctor.map(listOfNumbers)(_.toString)
+    val expectedMorphism = List("1", "2", "3")
+    assert(listOfStrings == expectedMorphism)
   }
 }
