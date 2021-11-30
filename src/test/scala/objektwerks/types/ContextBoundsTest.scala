eaeef65a8fb6e013685155bfa6991106702eec6f
objektwerks <objektwerks@runbox.com>
2021-11-30 06:47:49 -0500

    added context bounds test

---
+++ src/test/scala/objektwerks/types/ContextBoundsTest.scala
@@ -0,0 +1,10 @@
+package objektwerks.types
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class ContextBoundsTest extends AnyFunSuite with Matchers {
+  test("bounds") {
+    // TODO
+  }
+}
