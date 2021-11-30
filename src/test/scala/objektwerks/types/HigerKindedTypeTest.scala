220b9ab6f4f5ca04024220149659378e7357efad
objektwerks <objektwerks@runbox.com>
2021-11-30 06:49:03 -0500

    added higher kinded type test

---
+++ src/test/scala/objektwerks/types/HigerKindedTypeTest.scala
@@ -0,0 +1,10 @@
+package objektwerks.types
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class HigerKindedTypeTest extends AnyFunSuite with Matchers {
+  test("bounds") {
+    // TODO
+  }
+}
