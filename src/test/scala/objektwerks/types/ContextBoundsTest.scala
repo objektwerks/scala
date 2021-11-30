4b4d3c68a9302dc2f16f587453430faae7b1e42e
objektwerks <objektwerks@runbox.com>
2021-11-30 06:49:49 -0500

    renamed tests

--- src/test/scala/objektwerks/types/ContextBoundsTest.scala
+++ src/test/scala/objektwerks/types/ContextBoundsTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class ContextBoundsTest extends AnyFunSuite with Matchers {
-  test("bounds") {
+  test("context bounds") {
     // TODO
   }
 }
--- src/test/scala/objektwerks/types/HigerKindedTypeTest.scala
+++ src/test/scala/objektwerks/types/HigerKindedTypeTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class HigerKindedTypeTest extends AnyFunSuite with Matchers {
-  test("bounds") {
+  test("higher kinded") {
     // TODO
   }
 }
