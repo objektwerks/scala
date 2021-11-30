da8391c4c4dca644ed1b7817196e8e06758d962a
objektwerks <objektwerks@runbox.com>
2021-11-30 07:39:44 -0500

    refactored context bounds test

--- src/test/scala/objektwerks/types/ContextBoundsTest.scala
+++ src/test/scala/objektwerks/types/ContextBoundsTest.scala
@@ -4,12 +4,12 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class ContextBoundsTest extends AnyFunSuite with Matchers {
-  test("context bounds") {
-    def maximum[A: Ordering](a: A, b: A): A = {
-      val ordering = implicitly[Ordering[A]]
-      ordering.max(a, b)
-    }
+  def maximum[A: Ordering](a: A, b: A): A = {
+    val ordering = implicitly[Ordering[A]]
+    ordering.max(a, b)
+  }
 
+  test("context bounds") {
     maximum( 1, 2 ) shouldBe 2
   }
 }
